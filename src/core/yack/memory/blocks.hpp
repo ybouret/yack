f6b746527f8ce12c4f8b4292775b5ee66b39a0f3
ybouret <yann.bouret@gmail.com>
2021-10-10 17:35:54 +0200

    wip

--- src/core/yack/memory/blocks.hpp
+++ src/core/yack/memory/blocks.hpp
@@ -38,24 +38,24 @@ namespace yack
             static const char      designation[];      //!< memory blocks
             typedef list_of<arena> niche_type;         //!< niche for table
 
-            explicit blocks();          //!< setup with capacity but not arena
-            virtual ~blocks() throw();  //!< cleanup
+            blocks();           //!< setup with capacity but not arena
+            ~blocks() throw();  //!< cleanup
 
             void *acquire(const size_t block_size);                           //!< acquire block
             void  release(void *block_addr, const size_t block_size) throw(); //!< release block
 
         private:
             YACK_DISABLE_COPY_AND_ASSIGN(blocks);
-            arena           *acquiring_arena; //!< last acquiring arena
-            arena           *releasing_arena; //!< last releasing arena
-            niche_type      *acquiring_niche; //!< last acquiring niche
-            niche_type      *releasing_niche; //!< last releasing niche
+            arena           *acquiring_arena;    //!< last acquiring arena
+            arena           *releasing_arena;    //!< last releasing arena
+            niche_type      *acquiring_niche;    //!< last acquiring niche
+            niche_type      *releasing_niche;    //!< last releasing niche
 
-            size_t           count; //!< number or recorded arenas
-            const size_t     tsize; //!< table size, power of two
-            const size_t     tmask; //!< table size - 1, for table access
-            const size_t     bytes; //!< table bytes
-            niche_type      *table; //!< table address
+            size_t           count;              //!< number or recorded arenas
+            const size_t     tsize;              //!< table size, power of two
+            const size_t     tmask;              //!< table size - 1, for table access
+            const size_t     bytes;              //!< table bytes
+            niche_type      *table;              //!< table address
             void            *impl_[arena_words]; //!< arena creator
 
             void   release_table() throw();
---
+++ src/core/yack/memory/small-objects.hpp
@@ -0,0 +1,13 @@
+//! \file
+
+#ifndef YACK_SMALL_OBJECTS_INCLUDED
+#define YACK_SMALL_OBJECTS_INCLUDED
+
+#include "yack/setup.hpp"
+
+namespace yack
+{
+    
+}
+#endif
+
