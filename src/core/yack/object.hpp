c4caca4f22756b4589a08554169e7835e1ef0cbb
Yann Bouret <yann.bouret@unice.fr>
2021-10-11 08:45:46 +0200

    wip

--- src/core/yack/object.hpp
+++ src/core/yack/object.hpp
@@ -35,7 +35,7 @@ namespace yack
         //
         // multiple objects new[]/delete[]
         //______________________________________________________________________
-        static void * operator new [](const size_t block_size);                              //!< new[block_size]
+        static void * operator new    [](const size_t block_size);                           //!< new[block_size]
         static void   operator delete [](void *block_addr, const size_t block_size) throw(); //!< delete[block_size]
 
         //______________________________________________________________________
@@ -44,6 +44,20 @@ namespace yack
         //______________________________________________________________________
         static void *operator new(size_t block_size, void *addr) throw(); //!< placement new
         static void  operator delete(void *, void *) throw();             //!< placement delete
+
+        //______________________________________________________________________
+        //
+        // helpers
+        //______________________________________________________________________
+
+        //! acquire one zombie block
+        template <typename T> static inline
+        T *zacquire() { return static_cast<T *>( object::operator new(sizeof(T)) ); }
+
+        //! release one zombie block
+        template <typename T> static inline
+        void zrelease(T *obj) throw() { assert(obj); object::operator delete(obj,sizeof(T)); }
+
 
     private:
         YACK_DISABLE_COPY_AND_ASSIGN(object);
