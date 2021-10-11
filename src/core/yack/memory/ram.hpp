e09035323800c0c1e618fb54c30a1386d8d5a503
ybouret <yann.bouret@gmail.com>
2021-10-11 21:56:28 +0200

    scarce

--- src/core/yack/memory/ram.hpp
+++ src/core/yack/memory/ram.hpp
@@ -1,4 +1,3 @@
-
 //! \file
 
 #ifndef YACK_MEMORY_RAM_INCLUDED
---
+++ src/core/yack/memory/scarce.hpp
@@ -0,0 +1,21 @@
+
+//! \file
+
+#ifndef YACK_MEMORY_SCARCE_INCLUDED
+#define YACK_MEMORY_SCARCE_INCLUDED 1
+
+#include "yack/setup.hpp"
+
+namespace yack
+{
+    
+    namespace memory
+    {
+        class scarce :
+    
+    }
+    
+}
+
+#endif
+
