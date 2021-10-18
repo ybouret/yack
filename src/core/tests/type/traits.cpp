c52e17ffc08c2fd74ebdef79464e03a6ac8b75f4
Yann Bouret <yann.bouret@unice.fr>
2021-10-18 08:55:18 +0200

    checking traits

--- src/core/tests/type/traits.cpp
+++ src/core/tests/type/traits.cpp
@@ -17,6 +17,16 @@ static inline void display_traits(const char *name)
     CHECK(pointer);
     CHECK(pointer_to_member);
     CHECK(reference);
+    CHECK(std_integer);
+    CHECK(std_unsigned);
+    CHECK(sys_integer);
+    CHECK(sys_unsigned);
+    CHECK(integral);
+    CHECK(std_real);
+    CHECK(arithmetic);
+    CHECK(fundamental);
+    CHECK(primitive);
+
 }
 
 #define DISPLAY_TRAITS(TYPE) display_traits<TYPE>( #TYPE )
@@ -25,6 +35,8 @@ YACK_UTEST(type_traits)
 {
     DISPLAY_TRAITS(int);
     DISPLAY_TRAITS(const int);
+    DISPLAY_TRAITS(int &);
+    DISPLAY_TRAITS(const int &);
 }
 YACK_UDONE()
 
