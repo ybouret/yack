a06163fbab711e7b7bef63c8922c59cf2e942b49
Yann Bouret <yann.bouret@unice.fr>
2021-11-09 17:53:59 +0100

    wip

---
+++ src/core/yack/string.cpp
@@ -0,0 +1,12 @@
+
+#include "yack/string.hpp"
+
+namespace yack
+{
+    string:: ~string() throw()
+    {
+    }
+
+    
+
+}
---
+++ src/core/yack/string.hpp
@@ -0,0 +1,28 @@
+#ifndef YACK_STRING_INCLUDED
+#define YACK_STRING_INCLUDED 1
+
+#include "yack/string/utf8.hpp"
+
+namespace yack
+{
+
+    class string
+    {
+    public:
+        explicit string();
+        virtual ~string() throw();
+
+    private:
+        size_t  width;  //!< max count
+        size_t  count;  //!< UTF-8
+        utf8   *entry;  //!< entry[1..count]
+        size_t  c_room; //!< c_room = 1 + 4 * count
+        size_t  c_maxi; //!< c_maxi=c_room-1
+        size_t  c_size; //!< c_size<=c_maxi
+        char   *c_addr; //!< buffer
+    };
+
+}
+
+#endif
+
