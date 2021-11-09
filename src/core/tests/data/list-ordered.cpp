849a5f7c383e77055a86ccfcc2dbf6f20db57c3d
Yann Bouret <yann.bouret@unice.fr>
2021-11-09 13:53:10 +0100

    wip

--- src/core/tests/data/list-ordered.cpp
+++ src/core/tests/data/list-ordered.cpp
@@ -37,7 +37,7 @@ YACK_UTEST(data_list_ordered)
 
     randomized::rand_ ran;
 
-    int keys[10];
+    int keys[100];
     const size_t knum = sizeof(keys)/sizeof(keys[0]);
 
     for(size_t i=0;i<knum;++i) keys[i] = int(i)*2;
@@ -54,20 +54,39 @@ YACK_UTEST(data_list_ordered)
             YACK_ASSERT(!ordered_list::search(L,key,Node::Compare,node));
             if(node)
             {
-                std::cerr << "insert " << key << " after " << node->code << std::endl;
+                //std::cerr << "insert " << key << " after " << node->code << std::endl;
                 L.insert_after(node, new Node(key) );
             }
             else
             {
-                std::cerr << "push front " << key << std::endl;
+                //std::cerr << "push front " << key << std::endl;
                 L.push_front( new Node(key) );
             }
         }
 
-        for(const Node *node=L.head;node;node=node->next)
+        for(const Node *node=L.head;node->next;node=node->next)
         {
-            std::cerr << node->code << std::endl;
+            assert(node->code<node->next->code);
         }
+
+
+        for(size_t i=0;i<2*knum;++i)
+        {
+            const int key  = int(i);
+            Node     *node = NULL;
+            if(0==(i&1))
+            {
+                YACK_ASSERT(ordered_list::search(L,key,Node::Compare,node));
+                YACK_ASSERT(node);
+                YACK_ASSERT(key==node->code);
+            }
+            else
+            {
+                YACK_ASSERT(!ordered_list::search(L,key,Node::Compare,node));
+            }
+        }
+
+
     }
 
 }
