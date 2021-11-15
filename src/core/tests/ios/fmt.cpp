6b928f18bb7e1a6bd2ad3eec4270b9be67515c1b
Yann Bouret <yann.bouret@unice.fr>
2021-11-15 13:37:38 +0100

    wip

--- src/core/tests/ios/fmt.cpp
+++ src/core/tests/ios/fmt.cpp
@@ -10,39 +10,43 @@ YACK_UTEST(ios_fmt)
 {
 
     {
-        uint8_t a = 0; std::cerr << ios::hexa(a) << std::endl;
-        a = 0x0a;      std::cerr << ios::hexa(a) << std::endl;
-        a = 0x12;      std::cerr << ios::hexa(a) << std::endl;
-    }
+        ios::ocstream os("fmt.dat");
 
-    {
-        uint16_t a = 0; std::cerr << ios::hexa(a) << std::endl;
-        a = 0x0a;       std::cerr << ios::hexa(a) << std::endl;
-        a = 0x12;       std::cerr << ios::hexa(a) << std::endl;
-        a = 0x123;      std::cerr << ios::hexa(a) << std::endl;
-        a = 0x1234;     std::cerr << ios::hexa(a) << std::endl;
+        {
+            uint8_t a = 0; std::cerr << ios::hexa(a) << std::endl; os << ios::hexa(a) << '\n';
+            a = 0x0a;      std::cerr << ios::hexa(a) << std::endl; os << ios::hexa(a) << '\n';
+            a = 0x12;      std::cerr << ios::hexa(a) << std::endl; os << ios::hexa(a) << '\n';
+        }
 
-    }
+        {
+            uint16_t a = 0; std::cerr << ios::hexa(a) << std::endl; os << ios::hexa(a) << '\n';
+            a = 0x0a;       std::cerr << ios::hexa(a) << std::endl; os << ios::hexa(a) << '\n';
+            a = 0x12;       std::cerr << ios::hexa(a) << std::endl; os << ios::hexa(a) << '\n';
+            a = 0x123;      std::cerr << ios::hexa(a) << std::endl; os << ios::hexa(a) << '\n';
+            a = 0x1234;     std::cerr << ios::hexa(a) << std::endl; os << ios::hexa(a) << '\n';
 
-    {
-        uint32_t a = 0; std::cerr << ios::hexa(a) << std::endl;
-        a = 0x0a;       std::cerr << ios::hexa(a) << std::endl;
-        a = 0x12;       std::cerr << ios::hexa(a) << std::endl;
-        a = 0x123;      std::cerr << ios::hexa(a) << std::endl;
-        a = 0x1234;     std::cerr << ios::hexa(a) << std::endl;
-        a = 0x12345;    std::cerr << ios::hexa(a) << std::endl;
-        a = 0x123456;   std::cerr << ios::hexa(a) << std::endl;
-        a = 0x1234567;  std::cerr << ios::hexa(a) << std::endl;
-        a = 0x12345678; std::cerr << ios::hexa(a) << std::endl;
-    }
-    std::cerr << std::endl;
+        }
 
-    {
-        uint8_t a = 0; std::cerr << ios::binary(a) << std::endl;
-        a = 0x01;      std::cerr << ios::binary(a) << std::endl;
-        a = 0x02;      std::cerr << ios::binary(a) << std::endl;
-        a = 0xab;      std::cerr << ios::binary(a) << std::endl;
-        a = 0x80;      std::cerr << ios::binary(a) << std::endl;
+        {
+            uint32_t a = 0; std::cerr << ios::hexa(a) << std::endl; os << ios::hexa(a) << '\n';
+            a = 0x0a;       std::cerr << ios::hexa(a) << std::endl; os << ios::hexa(a) << '\n';
+            a = 0x12;       std::cerr << ios::hexa(a) << std::endl; os << ios::hexa(a) << '\n';
+            a = 0x123;      std::cerr << ios::hexa(a) << std::endl; os << ios::hexa(a) << '\n';
+            a = 0x1234;     std::cerr << ios::hexa(a) << std::endl; os << ios::hexa(a) << '\n';
+            a = 0x12345;    std::cerr << ios::hexa(a) << std::endl; os << ios::hexa(a) << '\n';
+            a = 0x123456;   std::cerr << ios::hexa(a) << std::endl; os << ios::hexa(a) << '\n';
+            a = 0x1234567;  std::cerr << ios::hexa(a) << std::endl; os << ios::hexa(a) << '\n';
+            a = 0x12345678; std::cerr << ios::hexa(a) << std::endl; os << ios::hexa(a) << '\n';
+        }
+        std::cerr << std::endl;
+
+        {
+            uint8_t a = 0; std::cerr << ios::binary(a) << std::endl; os << ios::binary(a) << '\n';
+            a = 0x01;      std::cerr << ios::binary(a) << std::endl; os << ios::binary(a) << '\n';
+            a = 0x02;      std::cerr << ios::binary(a) << std::endl; os << ios::binary(a) << '\n';
+            a = 0xab;      std::cerr << ios::binary(a) << std::endl; os << ios::binary(a) << '\n';
+            a = 0x80;      std::cerr << ios::binary(a) << std::endl; os << ios::binary(a) << '\n';
+        }
     }
 
     {
