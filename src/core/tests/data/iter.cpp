fd80db20efbd1640cb77813c957e9baf53d42402
Yann Bouret <yann.bouret@unice.fr>
2021-11-16 17:26:20 +0100

    wip iter

--- src/core/tests/data/iter.cpp
+++ src/core/tests/data/iter.cpp
@@ -3,10 +3,53 @@
 
 using namespace yack;
 
+
 YACK_UTEST(data_iter)
 {
     std::cerr << iterating::direction_text(iterating::forward) << std::endl;
     std::cerr << iterating::direction_text(iterating::reverse) << std::endl;
+
+
+    int data[] = { 1, 2, 3, 4, 5 };
+    {
+        {
+            const iterating::linear<int,iterating::forward> end(data+5);
+            iterating::linear<int,iterating::forward>       cur(data+0);
+            while(cur!=end)
+            {
+                std::cerr << *(cur++) << std::endl;
+            }
+        }
+
+        {
+            const iterating::linear<const int,iterating::forward> end(data+5);
+            iterating::linear<const int,iterating::forward>       cur(data+0);
+            while(cur!=end)
+            {
+                std::cerr << *(cur++) << std::endl;
+            }
+        }
+
+        {
+            const iterating::linear<int,iterating::reverse> end(data-1);
+            iterating::linear<int,iterating::reverse>       cur(data+4);
+            while(cur!=end)
+            {
+                std::cerr << *(cur++) << std::endl;
+            }
+        }
+
+        {
+            const iterating::linear<const int,iterating::reverse> end(data-1);
+            iterating::linear<const int,iterating::reverse>       cur(data+4);
+            while(cur!=end)
+            {
+                std::cerr << *(cur++) << std::endl;
+            }
+        }
+
+    }
+
 
 }
 YACK_UDONE()
--- src/core/yack/container/iterator/linear.hpp
+++ src/core/yack/container/iterator/linear.hpp
@@ -4,12 +4,164 @@
 #define YACK_LINEAR_ITERATOR_INCLUDED 1
 
 #include "yack/container/iterator.hpp"
+#include "yack/type/args.hpp"
 
 namespace yack
 {
 
     namespace iterating
     {
+        //______________________________________________________________________
+        //
+        //
+        //! linear iterator
+        //
+        //______________________________________________________________________
+        template <typename T,direction D>
+        class linear
+        {
+        public:
+            //__________________________________________________________________
+            //
+            // types and definitions
+            //__________________________________________________________________
+            YACK_DECL_ARGS_(T,type); //!< aliases
+
+            //__________________________________________________________________
+            //
+            // C++
+            //__________________________________________________________________
+            inline ~linear()                throw() { ptr=0; }                        //!< cleanup
+            inline  linear(type *        _) throw() : ptr( (mutable_type*)_)     {}   //!< setup
+            inline  linear(const linear &_) throw() : ptr( (mutable_type*)_.ptr) {}   //!< copy
+            inline  linear&operator=(const linear &_) throw()                         //|
+            { ptr=(mutable_type *)_.ptr; return *this; }                              //!< assign
+
+            //__________________________________________________________________
+            //
+            //! prefix increment operator
+            //__________________________________________________________________
+            inline linear & operator++()
+            {
+                static const int2type<D> way = {}; incr(way); return *this;
+            }
+
+            //__________________________________________________________________
+            //
+            //! postfix increment operator
+            //__________________________________________________________________
+            inline linear  operator++(int)
+            {
+                static const int2type<D> way = {};
+                const linear             tmp = *this;
+                incr(way);  return tmp;
+            }
+
+            //__________________________________________________________________
+            //
+            //! prefix decrement operator
+            //__________________________________________________________________
+            inline linear & operator--()
+            {
+                static const int2type<D> way = {}; decr(way); return *this;
+            }
+
+            //__________________________________________________________________
+            //
+            //! postfix decrement operator
+            //__________________________________________________________________
+            inline linear  operator--(int)
+            {
+                static const int2type<D> way = {};
+                const linear tmp = *this; decr(way); return tmp;
+            }
+
+            //__________________________________________________________________
+            //
+            //! += n
+            //__________________________________________________________________
+            inline linear & operator+=(const ptrdiff_t n) throw()
+            {
+                static const int2type<D> way = {};
+                add(way,n); return *this;
+            }
+
+            //__________________________________________________________________
+            //
+            //! self + n
+            //__________________________________________________________________
+            inline friend bool operator+(const linear &lhs, const ptrdiff_t n) throw()
+            {
+                linear tmp = lhs; tmp += n; return tmp;
+            }
+
+            //__________________________________________________________________
+            //
+            //! -= n
+            //__________________________________________________________________
+            inline linear & operator-=(const ptrdiff_t n) throw()
+            {
+                static const int2type<D> way = {};
+                sub(way,n); return *this;
+            }
+
+            //__________________________________________________________________
+            //
+            //! self - n
+            //__________________________________________________________________
+            inline friend bool operator-(const linear &lhs, const ptrdiff_t n) throw()
+            {
+                linear tmp = lhs; tmp -= n; return tmp;
+            }
+
+
+
+            //__________________________________________________________________
+            //
+            //! equality
+            //__________________________________________________________________
+            inline friend bool operator==(const linear &lhs, const linear &rhs) throw()
+            {
+                return lhs.ptr == rhs.ptr;
+            }
+
+            //__________________________________________________________________
+            //
+            //! difference
+            //__________________________________________________________________
+            inline friend bool operator!=(const linear &lhs, const linear &rhs) throw()
+            {
+                return lhs.ptr != rhs.ptr;
+            }
+
+            //__________________________________________________________________
+            //
+            //! access
+            //__________________________________________________________________
+            inline type       & operator*()       throw() { assert(ptr); return *ptr; }
+
+            //__________________________________________________________________
+            //
+            //! access, const
+            //__________________________________________________________________
+            inline const_type & operator*() const throw() { assert(ptr); return *ptr; }
+
+        private:
+            mutable_type *ptr;
+            inline void incr(const int2type<forward> &) throw() { ++ptr; }
+            inline void incr(const int2type<reverse> &) throw() { --ptr; }
+            inline void decr(const int2type<forward> &) throw() { --ptr; }
+            inline void decr(const int2type<reverse> &) throw() { ++ptr; }
+
+            inline void add(const int2type<forward> &, const ptrdiff_t n) throw() { ptr+=n; }
+            inline void add(const int2type<reverse> &, const ptrdiff_t n) throw() { ptr-=n; }
+            inline void sub(const int2type<forward> &, const ptrdiff_t n) throw() { ptr-=n; }
+            inline void sub(const int2type<reverse> &, const ptrdiff_t n) throw() { ptr+=n; }
+
+        };
+
+
+
 
     }
 
