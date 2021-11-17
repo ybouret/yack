1f80f33e7e312c2febcc6d7a866c45683a14b3db
Yann Bouret <yann.bouret@unice.fr>
2021-11-17 10:02:59 +0100

    need to implment insertions

--- src/core/tests/data/hash-set.cpp
+++ src/core/tests/data/hash-set.cpp
@@ -4,8 +4,39 @@
 
 using namespace yack;
 
+namespace
+{
+    class dummy : public object
+    {
+    public:
+        const int k;
+
+        dummy(const int a) throw() : k(a)
+        {
+        }
+
+        dummy(const dummy &d) throw() : k(d.k)
+        {
+        }
+
+
+        ~dummy() throw()
+        {
+        }
+
+        const int & key() const throw() { return k; }
+
+    private:
+        YACK_DISABLE_ASSIGN(dummy);
+    };
+}
+
 YACK_UTEST(data_hash_set)
 {
+    {
+        hash_set<int,dummy> s;
 
+        s.search(5);
+    }
 }
 YACK_UDONE()
--- src/core/yack/associative/hash/set.hpp
+++ src/core/yack/associative/hash/set.hpp
@@ -1,23 +1,60 @@
-
 //! \file
 
 #ifndef YACK_HASH_SET_INCLUDED
 #define YACK_HASH_SET_INCLUDED 1
 
 #include "yack/container/associative.hpp"
+#include "yack/data/hash/table.hpp"
+#include "yack/associative/hash/key-hasher.hpp"
 
 namespace yack
 {
 
-    template <typename KEY, typename T>
+
+    template <
+    typename KEY,
+    typename T,
+    typename KEY_HASHER = key_hasher<KEY> >
     class hash_set : public associative<KEY,T>
     {
     public:
-        inline virtual ~hash_set() throw() {}
-        inline explicit hash_set() throw() {}
+        YACK_DECL_ARGS(T,type);
+        YACK_DECL_ARGS(KEY,key_type);
+        class node_type
+        {
+        public:
+            inline ~node_type() throw() {}
+            inline  node_type(param_type v) :
+            next(0), prev(0), val_(v)
+            {
+            }
 
+            inline type           & operator*()       throw() { return val_; }
+            inline const_type     & operator*() const throw() { return val_; }
+            inline const_key_type & key() const throw() { return val_.key(); }
+
+            node_type *next;
+            node_type *prev;
+        private:
+            YACK_DISABLE_COPY_AND_ASSIGN(node_type);
+            type           val_;
+        };
+
+        typedef kernel::hash_table<KEY,node_type> htable_type;
+
+        inline virtual ~hash_set() throw() {}
+        inline explicit hash_set() throw() : hash(), table() {}
+
+        const_type *search(param_key_type key) const throw()
+        {
+            const node_type *node = table.search(hash(key),key);
+            return node ? & (**node) : NULL;
+        }
+
+        mutable KEY_HASHER  hash;
     private:
         YACK_DISABLE_COPY_AND_ASSIGN(hash_set);
+        htable_type table;
     };
 
 }
