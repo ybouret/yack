0cf2835051ea05be7488e0d5cb4db7b29def2fc3
ybouret <yann.bouret@gmail.com>
2021-11-18 20:02:28 +0100

    wip tests

--- src/core/tests/data/key-selector.cpp
+++ src/core/tests/data/key-selector.cpp
@@ -8,12 +8,18 @@ using namespace yack;
 namespace
 {
     template <typename KEY>
-    static inline void check_key(const char *id)
+    static inline void check_key(const key_variety::type guess,
+                                 const char             *id)
     {
         static const key_variety::type kv = key_variety::cull<KEY>::variety;
         std::cerr << key_variety::text(kv) << " <== ";
         std::cerr << "KEY=<" << typeid(KEY).name() << "> = '" << id << "'";
-        std::cerr << std::endl;
+        if(guess!=kv)
+        {
+            throw exception("key_variety mismatch");
+        }
+        std::cerr << " [OK]" << std::endl;
+
     }
 
     
@@ -23,17 +29,28 @@ namespace
     
 }
 
-#define YACK_CHECK_KEY(TYPE) check_key<TYPE>(#TYPE); check_key<const TYPE>("const " #TYPE)
+#define YACK_CHECK_KEY(GUESS,TYPE) \
+check_key<TYPE>(key_variety::is_##GUESS,#TYPE);\
+check_key<const TYPE>(key_variety::is_##GUESS,"const " #TYPE)
 
 YACK_UTEST(data_key_selector)
 {
+    YACK_CHECK_KEY(integral_type,char);
+    YACK_CHECK_KEY(integral_type,int32_t);
+    YACK_CHECK_KEY(integral_type,void*);
+    YACK_CHECK_KEY(not_available,dummy);
+    YACK_CHECK_KEY(memory_buffer,digest);
+    YACK_CHECK_KEY(legacy_string,char *);
+
+#if 0
     YACK_CHECK_KEY(char);
     YACK_CHECK_KEY(int32_t);
     YACK_CHECK_KEY(dummy);
     YACK_CHECK_KEY(void*);
     YACK_CHECK_KEY(digest);
     YACK_CHECK_KEY(char *);
-
+#endif
+    
 }
 YACK_UDONE()
 
