d4b11f182f9aaa52536ac581b77d23887439ed70
Yann Bouret <yann.bouret@unice.fr>
2021-11-22 15:59:06 +0100

    wip

--- src/core/yack/string/string.hpp
+++ src/core/yack/string/string.hpp
@@ -23,8 +23,8 @@ namespace yack
         class string : public string_, public writable<T>
         {
         public:
-            using typename writable<T>::const_type;
-            using typename writable<T>::type;
+            using  typename writable<T>::const_type;
+            using  typename writable<T>::type;
 
             
             explicit string();                   //!< default empty
