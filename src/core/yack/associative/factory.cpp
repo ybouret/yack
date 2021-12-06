704ebace9543ab88fffa89c5c74fa9c346cc15d7
Yann Bouret <yann.bouret@unice.fr>
2021-12-06 10:30:42 +0100

    added missing file

---
+++ src/core/yack/associative/factory.cpp
@@ -0,0 +1,50 @@
+
+#include "yack/associative/factory.hpp"
+#include "yack/exception.hpp"
+#include "yack/system/rtti.hpp"
+#include "yack/string.hpp"
+
+namespace yack
+{
+    namespace kernel
+    {
+
+        factory:: ~factory() throw()
+        {
+        }
+
+        factory:: factory() throw()
+        {
+        }
+
+        void factory:: throw_multiple_key(const std::type_info &tid) const
+        {
+            static const char * const id = yack::rtti::name(tid)();
+            throw exception("multiple key for factory<%s>", id);
+        }
+
+
+        size_t factory::  check_valid_length(const char *key, const std::type_info &tid) const
+        {
+            static const char * const id = yack::rtti::name(tid)();
+            if(NULL==key)
+            {
+                throw exception("NULL key for for factory<%s>",id);
+            }
+            const size_t len = strlen(key);
+            if(len<=0)
+            {
+                throw exception("empty key for for factory<%s>",id);
+            }
+            return len;
+        }
+
+        void   factory:: throw_unknown_key(const std::type_info &tid) const
+        {
+            static const char * const id = yack::rtti::name(tid)();
+            throw exception("unknown key for for factory<%s>",id);
+        }
+
+
+    }
+}
