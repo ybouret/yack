9f6956e5ece8f62f5936e55d8141a490bbc14f9d
Yann Bouret <yann.bouret@unice.fr>
2021-12-14 08:03:56 +0100

    wip

--- src/core/tests/data/stats.cpp
+++ src/core/tests/data/stats.cpp
@@ -5,9 +5,27 @@ using namespace yack;
 
 YACK_UTEST(data_stats)
 {
-    const int arr[4] = { 1, 1, 1, 1 };
-    YACK_CHECK(1==statistical::average<int>::of_(arr,4));
-    
+    {
+        const int arr[4] = { 1, 1, 1, 1 };
+        YACK_CHECK(1==statistical::average<int>::of_(arr,4));
+    }
+
+    vector<double> temp;
+    vector<double> x;
+    x.push_back(2);
+    x.push_back(4);
+    const double mu   = statistical::average<double>::of(x);
+    const double sig2 = statistical::variance<double>::of(x,mu);
+    std::cerr <<  "mu   = "  << mu  << std::endl;
+    std::cerr <<  "sig2  = " << sig2 << std::endl;
+
+    const double mu_prime   = statistical::average<double>::of(x,temp);
+    const double sig2_prime = statistical::variance<double>::of(x,mu_prime,temp);
+    std::cerr <<  "mu    = "  << mu_prime   << std::endl;
+    std::cerr <<  "sig2  = "  << sig2_prime  << std::endl;
+
+
+
 }
 YACK_UDONE()
 
--- src/core/yack/sequence/stats.hpp
+++ src/core/yack/sequence/stats.hpp
@@ -5,15 +5,20 @@
 
 #include "yack/sequence/vector.hpp"
 #include "yack/sort/sum.hpp"
+#include <cmath>
 
 namespace yack
 {
     
     namespace statistical
     {
-        template <typename T>
-        struct average
+        //______________________________________________________________________
+        //
+        //! sample average
+        //______________________________________________________________________
+        template <typename T> struct average
         {
+            //! from iterator
             template <typename ITERATOR> static
             inline T of_(ITERATOR it, const size_t n)
             {
@@ -26,7 +31,8 @@ namespace yack
                 }
                 return sum/n;
             }
-            
+
+            //! from iterator with sorted sum
             template <typename ITERATOR, typename SEQUENCE> static
             inline T of_(ITERATOR it, const size_t n,SEQUENCE &temp)
             {
@@ -38,7 +44,78 @@ namespace yack
                 }
                 return sorted::sum(temp,sorted::by_abs_value)/n;
             }
-            
+
+            //! for a whole sequence
+            template <typename SEQUENCE> static
+            inline T of(SEQUENCE &seq)
+            {
+                return of_( seq.begin(), seq.size() );
+            }
+
+            //! for a while sequence, with sorted sum
+            template <typename SEQUENCE, typename SEQTEMP> static
+            inline T of(SEQUENCE &seq, SEQTEMP &temp)
+            {
+                return of_( seq.begin(), seq.size(), temp );
+            }
+
+
+        };
+
+
+        //______________________________________________________________________
+        //
+        //! sample variance
+        //______________________________________________________________________
+        template <typename T>
+        struct variance
+        {
+            //! from iterator
+            template <typename ITERATOR> static inline
+            T of_(ITERATOR it, const size_t n, const T ave)
+            {
+                assert(n>1);
+                T sum = 0, sum2 = 0;
+                for(size_t i=n;i>0;--i)
+                {
+                    const T del = *(it++) - ave;
+                    sum  += del;
+                    sum2 += del*del;
+                }
+
+                return  ((sum2 - (sum*sum)/n)/(n-1));
+            }
+
+            //! from iterator with sorted sum
+            template <typename ITERATOR, typename SEQUENCE> static inline
+            T of_(ITERATOR it, const size_t n, const T ave, SEQUENCE &temp)
+            {
+                assert(n>1);
+                temp.free();
+                for(size_t i=n;i>0;--i)
+                {
+                    const T  del = *(it++) - ave;
+                    const T  sqr = del*del;
+                    temp.push_back(sqr);
+                }
+                return  ( sorted::sum(temp,sorted::by_value) / (n-1) );
+            }
+
+
+            //! for a whole sequence
+            template <typename SEQUENCE> static
+            inline T of(SEQUENCE &seq, const T ave)
+            {
+                return of_( seq.begin(), seq.size(), ave );
+            }
+
+            //! for a whole sequence with sorted sum
+            template <typename SEQUENCE, typename SEQTEMP> static
+            inline T of(SEQUENCE &seq, const T ave, SEQTEMP &temp)
+            {
+                return of_( seq.begin(), seq.size(), ave, temp );
+            }
+
         };
         
         
