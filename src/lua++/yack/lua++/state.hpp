b52c80491f81216f0db7f41235cae6c909f6713a
Yann Bouret <yann.bouret@unice.fr>
2021-12-21 10:25:33 +0100

    wip

--- src/lua++/yack/lua++/state.hpp
+++ src/lua++/yack/lua++/state.hpp
@@ -16,17 +16,29 @@ namespace yack
 {
     namespace Lua
     {
+        //______________________________________________________________________
+        //
+        //
+        //! wrapper for lua_State
+        //
+        //______________________________________________________________________
         class State : public object, public counted
         {
         public:
-            virtual ~State() throw();
-            explicit State();
+            virtual ~State() throw(); //!< lua_close
+            explicit State();         //!< lua_newstate
 
         private:
             YACK_DISABLE_COPY_AND_ASSIGN(State);
             lua_State *L;
         };
 
+        //______________________________________________________________________
+        //
+        //
+        //! shared lua state
+        //
+        //______________________________________________________________________
         typedef arc_ptr<State> VM;
     }
 }
