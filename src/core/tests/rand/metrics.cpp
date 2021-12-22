2a15d0cfcde4e4ba03c6cab875fd0677596628f9
Yann Bouret <yann.bouret@unice.fr>
2021-12-22 15:37:02 +0100

    wip u32

--- src/core/tests/rand/metrics.cpp
+++ src/core/tests/rand/metrics.cpp
@@ -8,9 +8,17 @@
 
 #include <typeinfo>
 #include <cstring>
+#include <cstdio>
 
 using namespace yack;
 
+
+template <typename T> static inline
+void show(randomized::u32_to<T> &m, const uint32_t u)
+{
+    const T value = m.unit(u);
+    std::cerr << "\tunit(" << std::setw(10) << u << ") = " << value << std::endl;
+}
 
 template <typename T>
 static inline void show_metrics()
@@ -21,29 +29,42 @@ static inline void show_metrics()
     std::cerr << "\t-> " << ios::hexa(randomized::u32_to<T>::maxi) << std::endl;
 
     {
-        randomized::u32_to<T> m(1000000);
+        const uint32_t small=1000000;
+        randomized::u32_to<T> m(small);
         std::cerr << "compacted: " << m.compacted << std::endl;
         std::cerr << "unit_deno: " << m.unit_deno << std::endl;
-        uint32_t u = 10;
-        std::cerr << m.unit(u) << std::endl;
+        std::cerr << "symm_deno: " << m.symm_deno << std::endl;
+        std::cerr << "unit:" << std::endl;
+        show(m,0);
+        show(m,small/2);
+        show(m,small);
+        YACK_CHECK(T(1)-m.unit(small)>0);
+
     }
 
     {
-        randomized::u32_to<T> m(0xffffffff);
+        const uint32_t big=0xffffffff;
+        randomized::u32_to<T> m(big);
         std::cerr << "compacted: " << m.compacted << std::endl;
         std::cerr << "unit_deno: " << m.unit_deno << std::endl;
-        uint32_t u = 10;
-        std::cerr << m.unit(u) << std::endl;
+        std::cerr << "symm_deno: " << m.symm_deno << std::endl;
+        std::cerr << "unit:" << std::endl;
+        show(m,0);
+        show(m,big/2);
+        show(m,big);
+        YACK_CHECK(T(1)-m.unit(big)>0);
     }
 
+    std::cerr << std::endl;
 
 }
 
 YACK_UTEST(rand_metrics)
 {
-    show_metrics<float>();
-    show_metrics<double>();
     show_metrics<long double>();
+    show_metrics<double>();
+    show_metrics<float>();
+
 }
 YACK_UDONE()
 
--- src/core/yack/randomized/u32-to.hpp
+++ src/core/yack/randomized/u32-to.hpp
@@ -35,10 +35,13 @@ namespace yack
 
             //! setup
             inline u32_to(const uint32_t umax) throw() :
+            unit_proc(0),
+            symm_proc(0),
             user_maxi(umax),
             compacted(user_maxi>maxi),
             unit_deno(0),
-            unit_proc(0)
+            half_maxi(0),
+            symm_deno(0)
             {
                 setup();
             }
@@ -58,11 +61,13 @@ namespace yack
             //
             // members
             //__________________________________________________________________
+            proc            unit_proc; //!< pre-computed method
+            proc            symm_proc; //!< pre-computed method
             const uint32_t  user_maxi; //!< user maxi value
             const bool      compacted; //!< user_maxi>maxi
-            const T         unit_deno; //!< pre-computed denominator
-            proc            unit_proc; //!< pre-computed method
-
+            const T         unit_deno; //!< pre-computed denominator, 'maxi'+1.0
+            const uint32_t  half_maxi; //!< pre-compted half maxi
+            const T         symm_deno; //!< pre-computed denominator, 'half_maxi'+0.5
          
 
 
@@ -72,18 +77,25 @@ namespace yack
             inline void setup() throw()
             {
                 static const T one(1);
+                static const T half(0.5);
                 if(compacted)
                 {
                     // limited by maxi
                     coerce(unit_deno) = static_cast<T>(maxi)+one;
+                    coerce(half_maxi) = maxi>>1;
                     unit_proc         = & u32_to<T>::unit_proc_compacted;
+                    symm_proc         = & u32_to<T>::symm_proc_compacted;
                 }
                 else
                 {
                     // only limited by user's maxi
                     coerce(unit_deno) = static_cast<T>(user_maxi)+one;
+                    coerce(half_maxi) = user_maxi>>1;
                     unit_proc         = & u32_to<T>::unit_proc_all_range;
+                    symm_proc         = & u32_to<T>::symm_proc_all_range;
+
                 }
+                coerce(symm_deno) = static_cast<T>(half_maxi)+half;
             }
 
             inline T unit_proc_all_range(const uint32_t u) const throw()
@@ -94,7 +106,25 @@ namespace yack
                 return (static_cast<T>(u)+half)/unit_deno;
             }
 
+            inline T symm_proc_all_range(const uint32_t u) const throw()
+            {
+                assert(user_maxi<=maxi);
+                assert(u<=user_maxi);
+                return ( (u<half_maxi) ? -static_cast<T>(half_maxi-u) : static_cast<T>(u-half_maxi) )/symm_deno;
+            }
+
+
             inline T unit_proc_compacted(const uint32_t u) const throw()
+            {
+                static const T half(0.5);
+                assert(user_maxi>maxi);
+                assert(u<=user_maxi);
+                const uint32_t U = static_cast<uint32_t>( (uint64_t(u) * uint64_t(maxi))/uint64_t(user_maxi) );
+                return (half+static_cast<T>(U))/unit_deno;
+            }
+
+
+            inline T symm_proc_compacted(const uint32_t u) const throw()
             {
                 static const T half(0.5);
                 assert(user_maxi>maxi);
