2f4b1c759f2ea48559be75444a5a8c0e80955d8d
Yann Bouret <yann.bouret@unice.fr>
2021-12-23 08:56:10 +0100

    wip

--- src/core/yack/system/seed.cpp
+++ src/core/yack/system/seed.cpp
@@ -1,21 +1,35 @@
 
 #include "yack/system/seed.hpp"
 #include "yack/system/pid.hpp"
+#include "yack/hashing/sha1.hpp"
+#include "yack/hashing/to.hpp"
 #include <ctime>
-#include "yack/hashing/des64.hpp"
 
 namespace yack
 {
+    namespace
+    {
+        struct seed_stamp
+        {
+            process_id::type seed_proc;
+            time_t           seed_time;
+            size_t           seed_word;
+
+            inline seed_stamp * next() throw()
+            {
+                seed_time = time(NULL);
+                seed_word++;
+                return this;
+            }
+
+        };
+    }
+
     uint64_t system_seed:: get64() throw()
     {
-        hashing::des64  h;
-        static uint64_t m = 0;
-        uint32_t lword = h.mix( process_id::get() );
-        uint32_t rword = h.mix( ++m + time(NULL)  );
-        h(lword,rword);
-        uint64_t ans = lword;
-        ans <<= 32;
-        ans |= rword;
-        return ans;
+
+        static hashing::sha1 hash;
+        static seed_stamp    mark = { process_id::get(), 0, 0 };
+        return hashing::to<uint64_t>(hash,mark.next(),sizeof(mark));
     }
 }
