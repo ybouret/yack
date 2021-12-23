b1d25980e365960574a8681df0a105c7403ec9d2
Yann Bouret <yann.bouret@unice.fr>
2021-12-23 09:50:55 +0100

    clean

---
+++ etc/make-png.sh
@@ -0,0 +1,19 @@
+echo "[PNG] making subdirs"
+mkdir -p build/png
+cd build
+echo "[PNG] configuring..."
+../configure --enable-shared=no --enable-hardware-optimizations=no --with-binconfigs=no
+echo "[PNG] buidling..."
+make -sj4
+echo "[PNG] copy headers..."
+cp ../*.h png 
+cp ./*.h png 
+echo "[PNG] copy sources..."
+for f in `ar -t .libs/libpng16.a | grep png`;
+do
+	#echo $f;
+	s=`basename $f .o`.c;
+	echo $s;
+	cp ../$s png 
+done
+
