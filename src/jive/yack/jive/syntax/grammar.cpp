b4b9a02ddd691e06ba8e6603f4183b74289d9da1
Yann Bouret <yann.bouret@unice.fr>
2021-12-24 11:01:58 +0100

    wip

---
+++ src/jive/yack/jive/syntax/grammar.cpp
@@ -0,0 +1,32 @@
+#include "yack/jive/syntax/grammar.hpp"
+#include "yack/exception.hpp"
+
+namespace yack
+{
+    namespace jive
+    {
+        namespace syntax
+        {
+            grammar:: ~grammar() throw()
+            {
+            }
+
+            void grammar:: declare(rule *r)
+            {
+                assert(r);
+                assert(!rules.owns(r));
+                auto_ptr<rule> guard(r);
+                const string  &id = *(r->name);
+                for(const rule *p=rules.head;p;p=p->next)
+                {
+                    if( *(p->name) == id ) throw exception("[%s] multiple rule <%s>", (*lang)(), id());
+                }
+                rules.push_back( guard.yield() );
+            }
+            
+        }
+
+    }
+
+}
+
--- src/jive/yack/jive/syntax/grammar.hpp
+++ src/jive/yack/jive/syntax/grammar.hpp
@@ -22,10 +22,43 @@ namespace yack
             class grammar
             {
             public:
-                virtual ~grammar() throw();
+                //______________________________________________________________
+                //
+                // C++
+                //______________________________________________________________
+                virtual ~grammar() throw(); //!< cleanup
+
+                //! setup
+                template <typename LANG> inline
+                explicit grammar(const LANG &id) :
+                lang( tags::make(id) ), rules()
+                {
+                }
+
+                //______________________________________________________________
+                //
+                // methods
+                //______________________________________________________________
+                //! declare a new (derived) rule
+                template <typename RULE>
+                RULE &toto(RULE *r)
+                {
+                    assert(r);
+                    declare(r);
+                    return *r;
+                }
+
+                //______________________________________________________________
+                //
+                // members
+                //______________________________________________________________
+                const tag lang; //!< language name
 
             private:
                 YACK_DISABLE_COPY_AND_ASSIGN(grammar);
+                cxx_list_of<rule> rules;
+                void       declare(rule *);
+
             };
 
         }
