1cd78ea03f4cfbf1040b7f91ac48a8f6bc804250
Yann Bouret <yann.bouret@unice.fr>
2021-12-27 12:51:51 +0100

    wip solving ode

--- src/math/tests/ode/explicit.cpp
+++ src/math/tests/ode/explicit.cpp
@@ -6,6 +6,7 @@
 #include "yack/system/rtti.hpp"
 #include "yack/string/cxx-name.hpp"
 #include "yack/ios/ocstream.hpp"
+#include "yack/type/utils.hpp"
 
 using namespace yack;
 using namespace math;
@@ -50,6 +51,8 @@ namespace
         ode::driver<T>                   odeint;
         ode::rk45<T>                     step( new ode::cash_karp<T>() );
 
+        std::cerr << filename << std::endl;
+
         T         h    = 1e-3;
         T         x    = 0;
         const T   xmax = 2.2;
@@ -65,11 +68,81 @@ namespace
             const T x_end = (i*xmax)/nmax;
             odeint(step,y,x,x_end,1e-4,h,drvs,NULL);
             x=x_end;
-            std::cerr << "@" << x << " " << y << std::endl;
+            //std::cerr << "@" << x << " " << y << std::endl;
             ios::ocstream::echo(filename,"%.15g %.15g\n", double(x), double(y[1]));
         }
-        
+
     }
+
+    template <typename T>
+    static inline void solveCos()
+    {
+        string       id = "cos-" + rtti::name<T>() + ".dat";
+        const string filename = cxx_name::of(id);
+        problem<T> pb = { 4.0 };
+        typename ode::named<T>::equation drvs(&pb,&problem<T>::dCos);
+        ode::driver<T>                   odeint;
+        ode::rk45<T>                     step( new ode::cash_karp<T>() );
+
+        std::cerr << filename << std::endl;
+
+        T         h    = 1e-3;
+        T         x    = 0;
+        const T   xmax = 22.2;
+        size_t    nmax = 200;
+        vector<T> y(2);
+        y[1] = 1;
+
+        ios::ocstream::overwrite(filename);
+        ios::ocstream::echo(filename,"%.15g %.15g %.15g \n", double(x), double(y[1]), double(y[2]));
+
+        for(size_t i=1;i<=nmax;++i)
+        {
+            const T x_end = (i*xmax)/nmax;
+            odeint(step,y,x,x_end,1e-4,h,drvs,NULL);
+            x=x_end;
+            //std::cerr << "@" << x << " " << y << std::endl;
+            ios::ocstream::echo(filename,"%.15g %.15g %.15g \n", double(x), double(y[1]), double(y[2]));
+        }
+
+    }
+
+
+    template <typename T>
+    static inline void solvePrj()
+    {
+        string       id = "prj-" + rtti::name<T>() + ".dat";
+        const string filename = cxx_name::of(id);
+        problem<T> pb = { 4.0 };
+        typename ode::named<T>::equation drvs(&pb,&problem<T>::dCos);
+        typename ode::named<T>::callback proc(&pb,&problem<T>::Prj);
+        ode::driver<T>                   odeint;
+        ode::rk45<T>                     step( new ode::cash_karp<T>() );
+
+        std::cerr << filename << std::endl;
+
+        T         h    = 1e-3;
+        T         x    = 0;
+        const T   xmax = 22.2;
+        size_t    nmax = 100;
+        vector<T> y(2);
+        y[1] = 1;
+
+        ios::ocstream::overwrite(filename);
+        ios::ocstream::echo(filename,"%.15g %.15g %.15g \n", double(x), double(y[1]), double(y[2]));
+
+        for(size_t i=1;i<=nmax;++i)
+        {
+            const T x_end = (i*xmax)/nmax;
+            odeint(step,y,x,x_end,1e-4,h,drvs,&proc);
+            x=x_end;
+            //std::cerr << "@" << x << " " << y << std::endl;
+            ios::ocstream::echo(filename,"%.15g %.15g %.15g \n", double(x), double(y[1]), double(y[2]));
+        }
+
+    }
+
+
 
 }
 
@@ -77,6 +150,8 @@ YACK_UTEST(explicit)
 {
 
     solveExp<float>();
+    solveCos<float>();
+    solvePrj<float>();
 
 
 
