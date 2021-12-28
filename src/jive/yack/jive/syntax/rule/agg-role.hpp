97b3466a9c4444060a479387d96fc453bbf81d36
Yann Bouret <yann.bouret@unice.fr>
2021-12-28 15:44:55 +0100

    wip

---
+++ src/jive/yack/jive/syntax/rule/agg-role.hpp
@@ -0,0 +1,26 @@
+
+//! \file
+
+#ifndef YACK_JIVE_SYNTAX_AGG_ROLE_INCLUDED
+#define YACK_JIVE_SYNTAX_AGG_ROLE_INCLUDED 1
+
+
+namespace yack
+{
+    namespace jive
+    {
+        namespace syntax
+        {
+
+            //! role for an aggregate
+            enum agg_role
+            {
+                toto
+            };
+
+        }
+
+    }
+}
+
+#endif
