876649573c721447d754971a3d76929acabcadd0
Yann Bouret <yann.bouret@unice.fr>
2022-01-06 09:16:25 +0100

    wip fit

--- src/math/yack/math/fit/least-squares.hpp
+++ src/math/yack/math/fit/least-squares.hpp
@@ -4,6 +4,7 @@
 #define YACK_FIT_LEAST_SQUARES_INCLUDED 1
 
 #include "yack/math/fit/sample.hpp"
+#include "yack/sequence/arrays.hpp"
 
 namespace yack
 {
@@ -15,20 +16,41 @@ namespace yack
             template <
             typename ABSCISSA,
             typename ORDINATE>
-            class least_squares 
+            class least_squares :
+            public large_object,
+            public arrays_of<ORDINATE>
             {
             public:
-                typedef sample<ABSCISSA,ORDINATE>              sample_type;
-                typedef typename derivative<ORDINATE>::pointer drvs_type;
+                typedef sample<ABSCISSA,ORDINATE>                sample_type;
+                typedef typename derivative<ORDINATE>::pointer   drvs_type;
+                typedef arrays_of<ORDINATE>                      tableaux;
+                typedef typename arrays_of<ORDINATE>::array_type array_type;
 
                 inline virtual ~least_squares() throw() {}
-                inline explicit least_squares() throw() {}
+                inline explicit least_squares() :
+                large_object(),
+                tableaux(4,0),
+                step( this->next() ),
+                curv(),
+                drvs()
+                {
+                }
 
-                
 
+
+                array_type          &step; //!< current step
+                matrix<ORDINATE>     curv; //!< modified matrix
+                derivative<ORDINATE> drvs; //!< derivative
 
             private:
                 YACK_DISABLE_COPY_AND_ASSIGN(least_squares);
+
+                void initialize(const size_t npar)
+                {
+                    this->make(npar);
+                    curv.make(npar,npar);
+                }
+
             };
 
         }
