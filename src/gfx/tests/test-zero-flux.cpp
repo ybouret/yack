d531b526b187930d48a06db2390989dec43e9f18
Yann Bouret <yann.bouret@unice.fr>
2022-02-25 11:28:21 +0100

    wip gfx

--- src/gfx/tests/test-zero-flux.cpp
+++ src/gfx/tests/test-zero-flux.cpp
@@ -9,7 +9,17 @@ using namespace graphic;
 
 YACK_UTEST(zero_flux)
 {
+    for(unit_t width=1;width<=10;++width)
+    {
+        const zero_flux zf(width);
+        for(unit_t x=-1000;x<=1000;++x)
+        {
+            const unit_t i=zf(x);
+            YACK_ASSERT(i>=0);
+            YACK_ASSERT(i<width);
 
+        }
+    }
 }
 YACK_UDONE()
 
