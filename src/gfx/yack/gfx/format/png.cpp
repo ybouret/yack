3ff91f98f57a12a4080ed97f87702fc3c6d5e4bc
Yann Bouret <yann.bouret@unice.fr>
2022-03-10 16:23:22 +0100

    wip png

---
+++ src/gfx/yack/gfx/format/png.cpp
@@ -0,0 +1,12 @@
+
+#include "yack/gfx/format/png.hpp"
+#include "yack/exception.hpp"
+
+namespace yack
+{
+    namespace graphic
+    {
+        
+
+    }
+}
---
+++ src/gfx/yack/gfx/format/png.hpp
@@ -0,0 +1,24 @@
+//! \file
+
+#ifndef YACK_GFX_FORMAT_PNG_INCLUDED
+#define YACK_GFX_FORMAT_PNG_INCLUDED 1
+
+#include "yack/gfx/rgb.hpp"
+#include "png.h"
+
+namespace yack
+{
+    namespace graphic
+    {
+
+        struct png_format
+        {
+            
+
+        };
+
+
+    }
+}
+
+#endif
