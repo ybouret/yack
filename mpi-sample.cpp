7db000ca603f82094f0189c3775dace5df884e9f
Yann Bouret <yann.bouret@unice.fr>
2022-04-04 13:36:42 +0200

    wip mpi

---
+++ mpi-sample.cpp
@@ -0,0 +1,13 @@
+#include <mpi.h>
+
+int main(int argc, char **argv )
+{
+	int required = MPI_THREAD_SINGLE;
+	int provided = 0;
+	int res = MPI_Init_thread(&argc,&argv,required,&provided);
+	if(res!=MPI_SUCCESS)
+	{
+	}
+	MPI_Finalize();
+	return 0;
+}
