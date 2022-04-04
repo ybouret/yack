1cd9ff3eeb42d49291609db6cc0e31fd28b602c5
Yann Bouret <yann.bouret@unice.fr>
2022-04-04 12:57:35 +0200

    need to write gaussj

--- src/math/yack/math/algebra/gauss-jordan.hpp
+++ src/math/yack/math/algebra/gauss-jordan.hpp
@@ -4,6 +4,7 @@
 #define YACK_MATH_GAUSSJ_INCLUDED 1
 
 #include "yack/container/matrix.hpp"
+#include "yack/math/api.hpp"
 
 namespace yack
 {
@@ -11,7 +12,34 @@ namespace yack
     {
         template <typename T> struct gauss_jordan
         {
-            
+            YACK_DECL_ARGS_(T,type);
+            typedef typename scalar_for<type>::type scalar_type;
+
+            static inline
+            bool build(matrix<T> &a, writable<T> &b)
+            {
+                assert( a.is_square() );
+                assert( b.size() >= a.rows );
+                const size_t n = a.rows;
+                for(size_t im=1,i=2;i<=n;++i,++im)
+                {
+                    const T &piv = a[im][im];
+                    {
+                        const scalar_type apiv = mod2_of(piv);
+                        if(apiv<=0) return false;
+                    }
+                    for(size_t k=i;k<=n;++k)
+                    {
+                        const_type coef = a[k][im] / piv;
+                        a[k][im] = 0;
+                        for(size_t j=n;j>k;--j) a[k][j] -= coef * a[im][k];
+                        b[k] -= coef * b[im];
+                    }
+                }
+
+                return true;
+            }
+
         };
     }
 }
