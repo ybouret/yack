45db097e163781fb79a5ae98f31c4e5bb3b7c84f
Yann Bouret <yann.bouret@unice.fr>
2022-05-02 08:35:56 +0200

    typos

--- src/gfx/yack/gfx/bitmap.cpp
+++ src/gfx/yack/gfx/bitmap.cpp
@@ -20,8 +20,8 @@ namespace yack
         bitmap:: bitmap(const unit_t W, const unit_t H, const unit_t D) :
         article(),
         metrics(W,H,D),
-        data( new bitfield(n*d) ),
-        rows( new bitrows(*this,data->entry))
+        data( new bitfield(n*d)              ),
+        rows( new bitrows(*this,data->entry) )
         {
 
         }
--- src/gfx/yack/gfx/format/png.cpp
+++ src/gfx/yack/gfx/format/png.cpp
@@ -184,58 +184,9 @@ namespace yack
 
 
             //class png_writer;
-
-
-            class PNG_Mem
-            {
-            public:
-                const size_t w;
-                const size_t h;
-                const size_t d;
-                const size_t s;
-                png_bytep   *row;
-
-                inline ~PNG_Mem() throw()
-                {
-                    static memory::allocator &mgr = memory_allocator::location();
-                    mgr.release(wksp,wlen);
-                }
-
-                inline PNG_Mem(const size_t width,
-                               const size_t height,
-                               const size_t depth) :
-                w(width),
-                h(height),
-                d(depth),
-                s(width*depth),
-                row(0),
-                wksp(0),
-                wlen(0)
-                {
-                    static memory::allocator &mgr = memory_allocator::instance();
-                    uint8_t *data  = 0;
-                    {
-                        memory::embed emb[] =
-                        {
-                            memory::embed(row,height),
-                            memory::embed(data,height*s)
-                        };
-                        wksp = YACK_MEMORY_EMBED(emb,mgr,wlen);
-                    }
-                    for(size_t j=0;j<h;++j,data+=s)
-                    {
-                        row[j] = data;
-                    }
-                }
-
-
-            private:
-                YACK_DISABLE_COPY_AND_ASSIGN(PNG_Mem);
-                void         *wksp;
-                size_t        wlen;
-            };
-
         }
+
+
 
         void png_load(const string &filename)
         {
