63881802547413125a6ddd7f78dae5e0c1e9f704
Yann Bouret <yann.bouret@unice.fr>
2022-05-06 08:45:44 +0200

    wip

---
+++ src/gfx/tests/test-jpg-io.cpp
@@ -0,0 +1,12 @@
+
+#include <stdlib.h>
+#include "../../yack/jpeg/tests/example.c"
+#include "yack/utest/run.hpp"
+
+YACK_UTEST(jpg_io)
+{
+
+}
+YACK_UDONE()
+
+
--- src/gfx/yack/gfx/format/options.cpp
+++ src/gfx/yack/gfx/format/options.cpp
@@ -61,5 +61,8 @@ namespace yack
 
         }
 
+        
+
+
     }
 }
