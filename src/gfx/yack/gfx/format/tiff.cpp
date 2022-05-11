62cb2c0270c1321f67f1c20f3816b96e0f1b2c79
Yann Bouret <yann.bouret@unice.fr>
2022-05-11 10:32:20 +0200

    wip

---
+++ src/gfx/yack/gfx/format/tiff.cpp
@@ -0,0 +1,13 @@
+
+#include "yack/gfx/format/tiff.hpp"
+//#include "tiffio.h"
+
+namespace yack
+{
+    namespace graphic
+    {
+
+    }
+
+}
+
---
+++ src/gfx/yack/gfx/format/tiff.hpp
@@ -0,0 +1,26 @@
+
+//! \file
+
+#ifndef YACK_GFX_FORMAT_PNG_INCLUDED
+#define YACK_GFX_FORMAT_PNG_INCLUDED 1
+
+#include "yack/gfx/format.hpp"
+
+namespace yack
+{
+    namespace graphic
+    {
+
+        class tiff_format : public format
+        {
+        public:
+            //static pixmap<rgba> load(const string &, const options *);
+            //static void         save(const pixmap<rgba> &, const string &, const options *opt);
+        };
+
+
+    }
+
+}
+
+#endif
