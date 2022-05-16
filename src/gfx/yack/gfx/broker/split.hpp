2962234b506d39fadb6610d2554d9001dca56a9f
ybouret <yann.bouret@gmail.com>
2022-05-16 18:37:22 +0200

    wip

---
+++ src/gfx/yack/gfx/broker/split.hpp
@@ -0,0 +1,30 @@
+
+//! \file
+
+#ifndef YACK_GFX_BROKER_SPLIT_INCLUDED
+#define YACK_GFX_BROKER_SPLIT_INCLUDED 1
+
+#include "yack/gfx/pixmaps.hpp"
+
+namespace yack
+{
+    namespace graphic
+    {
+        
+        struct broker_split
+        {
+            template <typename PIXEL,
+            typename T>
+            void build(pixmaps<T>          &target,
+                       const pixmap<PIXEL> &source)
+            {
+                
+            }
+        };
+        
+    }
+    
+}
+
+#endif
+
