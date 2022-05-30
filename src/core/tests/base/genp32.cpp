3c299673ce412e833276dbc683c08c326a6e01e7
Yann Bouret <yann.bouret@unice.fr>
2022-05-30 13:19:11 +0200

    wip gen32

--- src/core/tests/base/genp32.cpp
+++ src/core/tests/base/genp32.cpp
@@ -1,7 +1,11 @@
 #include "yack/arith/prime.hpp"
 #include "yack/string.hpp"
 #include "yack/ios/icstream.hpp"
+#include "yack/ios/gz/ostream.hpp"
+#include "yack/ios/ascii/convert.hpp"
+
 #include "yack/utest/run.hpp"
+#include <cstring>
 
 using namespace yack;
 
@@ -25,14 +29,18 @@ YACK_UTEST(genp32)
 {
     check16();
 
+    uint64_t prev = core::primes::h16;
+    uint64_t smax = 0;
+
     if(argc>1)
     {
         char            buffer[256];
         const string    fn = argv[1];
         ios::characters line;
-
-        ios::icstream fp(fn);
-        size_t        count = 0;
+        size_t          every = 0;
+        ios::icstream    fp(fn);
+        ios::gz::ostream zp("shift.gz",9);
+        size_t           count = 0;
         while(fp.gets(line))
         {
             memset(buffer,0,sizeof(buffer));
@@ -40,9 +48,28 @@ YACK_UTEST(genp32)
             {
                 //std::cerr << "[" << buffer << "]" << std::endl;
                 ++count;
+                const uint64_t curr = ios::ascii::convert::to<uint64_t>(buffer,"prime number");
+                YACK_ASSERT(curr>prev);
+                const uint64_t diff = curr-prev; YACK_ASSERT(0==(diff&1));
+                const uint64_t half = diff>>1;   YACK_ASSERT(half>0);
+                const uint64_t shft = half-1;
+                if(shft>smax)
+                {
+                    smax = shft;
+                }
+                if(shft>255) throw exception("shift is too high!!");
+                zp.write( uint8_t(shft) );
+                prev = curr;
+                if(++every>=100000)
+                {
+                    (std::cerr << '.').flush();
+                    every=0;
+                }
             }
         }
+        std::cerr << std::endl;
         std::cerr << "count=" << count << std::endl;
+        std::cerr << "smax =" << smax  << std::endl;
     }
 
 
