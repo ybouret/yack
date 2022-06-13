f2270c0f671954cc8708061b3cf9921a76be7012
Yann Bouret <yann.bouret@unice.fr>
2022-06-13 12:17:56 +0200

    wip nw

--- src/core/yack/sort/nw/nw.pl
+++ src/core/yack/sort/nw/nw.pl
@@ -3,38 +3,27 @@ use Algorithm::Networksort::Best qw(:all);
 
 my @algo = ( "bosenelson", "batcher" );
 
-#my $nw = nwsrt(inputs => $inputs, algorithm => $algorithm);
 for my $small (2..8)
 {
 	for my $alg (@algo)
 	{
 		my $nw = nwsrt(inputs => $small, algorithm => $alg);
-		print $nw->title(), "\n";
+		print $alg, $small, "\n";
 		print $nw, "\n\n";
 	}
 
 }
 
-exit(0);
- 
-my $inputs = 9;
- 
-#
-# First find if any networks exist for the input size.
-#
-my @nwkeys = nw_best_names($inputs);
- 
-#
-# For each sorting network, show the comparators.
-#
-for my $name (@nwkeys)
+my @large = (9..12);
+for my $inputs (@large)
 {
-    my $nw = nwsrt_best(name => $name);
- 
-    #
-    # Print the list, and print the graph of the list.
-    #
-    print $nw->title(), "\n", $nw->formatted(), "\n\n";
-    #print $nw->graph_text(), "\n\n";
+	my @nwkeys = nw_best_names($inputs);
+	for my $name (@nwkeys)
+	{	
+		#print $name,"\n";
+    		my $nw = nwsrt_best(name => $name);
+		print $name, "\n";
+                print $nw, "\n\n";
+	}
 }
 
