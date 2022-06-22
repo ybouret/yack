396441eb23414c10170549ee70a5d1716029d557
Yann Bouret <yann.bouret@unice.fr>
2022-06-22 10:07:07 +0200

    wip

--- src/math/yack/math/iota.hpp
+++ src/math/yack/math/iota.hpp
@@ -9,14 +9,26 @@ namespace yack {
 
     namespace math
     {
+        //______________________________________________________________________
+        //
+        //
         //! Inline Operations of Template Algebra
+        //
+        //______________________________________________________________________
         struct iota
         {
 
+            //------------------------------------------------------------------
+            //! target[1..target.size()] = source[1..target.size()]
+            //------------------------------------------------------------------
             template <typename TARGET, typename SOURCE> static inline
             void set(TARGET &target, SOURCE &source)
             {
+                assert(source.size()>=target.size());
+                for(size_t i=source.size();i>0;--i) target[i] = static_cast< typename TARGET::type >( source[i] );
             }
+
+
 
         };
 
