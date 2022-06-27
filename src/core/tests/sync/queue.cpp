7f1ba293a295c87929cefedbcccf8055fbe59c1f
Yann Bouret <yann.bouret@unice.fr>
2022-06-27 12:26:53 +0200

    need to rething sync queue

--- src/core/tests/sync/queue.cpp
+++ src/core/tests/sync/queue.cpp
@@ -1,4 +1,5 @@
 #include "yack/concurrent/queue/agent.hpp"
+#include "yack/sequence/cxx-array.hpp"
 #include "yack/utest/run.hpp"
 
 
@@ -12,17 +13,51 @@ namespace yack
         class agency
         {
         public:
+            size_t               live;
+            mutex                sync;
+            condition            cond;
+            cxx_array<condition> halt;
+            cxx_series<thread>   crew;
+
             virtual ~agency() throw()
             {
             }
 
-            explicit agency(const concurrent::topology &topo)
+            explicit agency(const concurrent::topology &topo) :
+            live(0),
+            sync(),
+            cond(),
+            halt(topo->size),
+            crew(topo->size)
             {
 
+                const size_t dims = topo->size;
+                while(crew.size()<dims)
+                {
+                    crew.add<thread::procedure,void*>(launch,this);
+                }
             }
 
         private:
             YACK_DISABLE_COPY_AND_ASSIGN(agency);
+
+            static void launch(void *args) throw()
+            {
+                assert(args);
+                static_cast<agency *>(args)->mission();
+            }
+
+            void mission() throw()
+            {
+                sync.lock();
+                ++live; std::cerr << "creating mission @" << live << std::endl;
+                assert(live<halt.size());
+                condition &waiting = halt[live];
+                waiting.wait(sync);
+            }
+
+
+
         };
 
     }
@@ -33,9 +68,10 @@ namespace yack
 YACK_UTEST(sync_queue)
 {
     YACK_SIZEOF(concurrent::agent);
-    const concurrent::topology topo(1);
+    const concurrent::topology topo;
 
     std::cerr << "topo=" << topo << std::endl;
+    concurrent::agency  A( topo );
     
 }
 YACK_UDONE()
