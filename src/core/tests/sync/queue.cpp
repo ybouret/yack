bca751d660a81fa488e307b6640f248137c7c51f
Yann Bouret <yann.bouret@unice.fr>
2022-06-29 09:44:17 +0200

    need to debug queue

--- src/core/tests/sync/queue.cpp
+++ src/core/tests/sync/queue.cpp
@@ -2,6 +2,7 @@
 #include "yack/concurrent/context.hpp"
 #include "yack/sequence/cxx-array.hpp"
 #include "yack/utest/run.hpp"
+#include "yack/memory/allocator/dyadic.hpp"
 
 
 using namespace yack;
@@ -15,6 +16,8 @@ namespace yack
         class pipeline
         {
         public:
+            static const char clid[];
+
             class worker : public context
             {
             public:
@@ -24,23 +27,91 @@ namespace yack
                 next(0),
                 prev(0),
                 host(boss),
+                cond(),
+                task(NULL),
                 ctx(size,rank),
-                thr(pipeline::entry,this)
+                thr(pipeline::entry,&host)
                 {
                 }
 
-                worker       *next;
-                worker       *prev;
-                pipeline     &host;
-                const context ctx;
-                thread        thr;
+                inline ~worker() throw()
+                {
+                }
+
+
+                worker       *next; //!< for lists
+                worker       *prev; //!< for lists
+                pipeline     &host; //!< origin
+                condition     cond; //!< auto-condition
+                void         *task; //!< task to do
+                const context ctx;  //!< context
+                const thread  thr;  //!< thread
+
+                static inline
+                worker *zalloc( size_t &capa )
+                {
+                    static memory::dyadic &mgr = memory::dyadic::instance();
+                    return static_cast<worker*>(mgr.acquire(capa,sizeof(worker)));
+                }
 
             private:
                 YACK_DISABLE_COPY_AND_ASSIGN(worker);
             };
 
-            explicit pipeline(const topology &);
-            virtual ~pipeline() throw() {}
+            explicit pipeline(const topology &topo) :
+            sync(),
+            gate(),
+            threads(topo->size),
+            zbytes_( threads ),
+            squad( worker::zalloc(zbytes_) ),
+            ready(0)
+            {
+                if(thread::verbose)
+                {
+                    std::cerr << clid << " zbytes=" << zbytes_ << std::endl;
+                }
+
+                {
+                    pipeline &host = *this;
+                    size_t    current = 0;
+                    while(current<threads)
+                    {
+                        try
+                        {
+                            new ( &squad[current] ) worker(host,threads,current);
+                            ++current;
+                        }
+                        catch(...)
+                        {
+                            --squad;
+                            finish(current);
+                        }
+                    }
+                }
+
+            PROBE:
+                if( sync.try_lock() )
+                {
+                    if(ready<threads)
+                    {
+                        std::cerr << "not ready..." << std::endl;
+                        sync.unlock();
+                        goto PROBE;
+                    }
+                    sync.unlock();
+                }
+                std::cerr << "ready!" << std::endl;
+                
+
+                // done
+                --squad;
+            }
+
+            virtual ~pipeline() throw()
+            {
+                std::cerr << clid << " done" << std::endl;
+                finish(threads);
+            }
 
             mutex     sync;
             condition gate;
@@ -58,6 +129,38 @@ namespace yack
             YACK_DISABLE_COPY_AND_ASSIGN(pipeline);
         };
 
+        const char pipeline:: clid[] = "concurrent::pipeline";
+
+        void pipeline:: zkill() throw()
+        {
+            static memory::allocator &mgr = memory::dyadic::location();
+            mgr.withdraw(squad,zbytes_);
+        }
+
+        void pipeline:: finish(size_t count) throw()
+        {
+            assert(count<=threads);
+            ++squad;
+            zkill();
+        }
+
+        void pipeline:: entry(void *args) throw()
+        {
+            assert(NULL!=args);
+            static_cast<pipeline *>(args)->cycle();
+        }
+
+        void pipeline:: cycle() throw()
+        {
+            sync.lock();
+            assert(ready<threads);
+            worker &agent = squad[ready++];
+
+            { std::cerr << clid << " " << agent.ctx << " @ready=" << ready << std::endl; }
+
+            agent.cond.wait(sync);
+
+        }
 
     }
 
@@ -68,8 +171,10 @@ YACK_UTEST(sync_queue)
 {
     //YACK_SIZEOF(concurrent::agent);
     const concurrent::topology topo;
+    concurrent::thread::verbose = true;
 
     std::cerr << "topo=" << topo << std::endl;
+    concurrent::pipeline Q(topo);
 
 }
 YACK_UDONE()
