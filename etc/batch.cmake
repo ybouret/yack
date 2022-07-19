7ecc19d43f8f796a425983a8ce3d61252be2917d
Yann Bouret <yann.bouret@unice.fr>
2022-07-19 09:33:51 +0200

    wip

--- etc/batch.cmake
+++ etc/batch.cmake
@@ -2,7 +2,20 @@ file(GLOB subdirs build*)
 #message( "[YACK] build: ${subdirs}" )
 
 foreach(subdir IN ITEMS ${subdirs})
-	message( "[YACK]  processing <${subdir}>" )
+	message( "[YACK] processing <${subdir}>" )
+	set(YACK "all")
+	if(EXISTS ${subdir}/yack.xcodeproj)
+		message( "[YACK]   found XCode" )
+		set(YACK "ALL_BUILD")
+	endif()
+
+	if(EXISTS ${subdir}/yack.sln)
+		message( "[YACK]   found Visual Studio" )
+		set(YACK "ALL_BUILD")
+	endif()
+
+	set(cmd "${CMAKE_COMMAND} --build ${subdir} --target ${YACK} --config Release" )
+	message( "[YACL]   executing '${cmd}'")
 endforeach()
 
 
