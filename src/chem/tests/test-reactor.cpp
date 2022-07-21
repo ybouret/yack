b68a12b61e64a4e0f2a8a89d8acf600426a12054
Yann Bouret <yann.bouret@unice.fr>
2022-07-21 16:02:52 +0200

    need to be more precise..

--- src/chem/tests/test-reactor.cpp
+++ src/chem/tests/test-reactor.cpp
@@ -1,16 +1,10 @@
 
-
-#include "yack/chem/mix/groups.hpp"
-#include "yack/chem/mix/couples.hpp"
+#include "yack/chem/reactor.hpp"
 #include "yack/chem/lua/equilibria.hpp"
-#include "yack/chem/library.hpp"
 #include "yack/system/env.hpp"
 #include "yack/utest/run.hpp"
-#include "yack/chem/mix/independency.hpp"
-#include "yack/ptr/auto.hpp"
+#include "yack/counting/comb.hpp"
 
-
-#include "yack/chem/reactor.hpp"
 
 using namespace yack;
 using namespace chemical;
@@ -18,6 +12,7 @@ using namespace chemical;
 YACK_UTEST(reactor)
 {
     entity::verbose = environment::flag("VERBOSE");
+    size_t            iter = 5;
     randomized::rand_ ran;
     library           lib;
     lua_equilibria    eqs;
@@ -33,7 +28,64 @@ YACK_UTEST(reactor)
     reactor        cs(lib,eqs,0.0);
     vector<double> C(cs.M,0);
 
-    
+    C.ld(0);
+
+    if(!cs.steady(C))
+    {
+        throw exception("cannot solve null phase space!!");
+    }
+    lib(std::cerr<<"Cend=",C);
+    cs.display(C);
+
+    vector<species *> active(cs.active.size,as_capacity);
+    for(const anode *node=cs.active.head;node;node=node->next)
+    {
+        active << & coerce(**node);
+    }
+    const size_t n = active.size();
+    for(size_t k=1;k<=n;++k)
+    {
+        combination comb(n,k);
+        do {
+            vector<species *> sub(k,as_capacity);
+            comb.extract(sub,active);
+            std::cerr << "---- using";
+            for(size_t i=1;i<=k;++i)
+            {
+                std::cerr << " " << sub[i]->name;
+            }
+            std::cerr << std::endl;
+
+            std::cerr << "[";
+            for(size_t it=0;it<iter;++it)
+            {
+                C.ld(0);
+                for(size_t i=k;i>0;--i)
+                {
+                    const size_t j = **sub[i];
+                    C[j] = species::concentration(ran);
+                }
+                (std::cerr << ".").flush();
+                //lib(std::cerr<<"Cini=",C);
+                if(!cs.steady(C))
+                {
+                    lib(std::cerr<<"Cini=",C);
+                    entity::verbose = true;
+                    (void)cs.steady(C);
+                    //cs.display(C);
+                    throw exception("Couldn't solve!");
+                }
+            }
+            std::cerr << "]" << std::endl;
+            std::cerr << std::endl;
+        }
+        while(comb.next());
+    }
+
+
+
+
+    return 0;
 
     for(size_t iter=0;iter<100;++iter)
     {
