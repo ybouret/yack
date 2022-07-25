161a4da085454bf11b904a2cff38e71dd5ca989f
Yann Bouret <yann.bouret@unice.fr>
2022-07-25 09:47:38 +0200

    wip

--- src/core/tests/sort/quick.cpp
+++ src/core/tests/sort/quick.cpp
@@ -1,63 +1,87 @@
 #include "yack/utest/run.hpp"
+#include "yack/sequence/vector.hpp"
+#include "yack/comparison.hpp"
+
+#include "../main.hpp"
+#include <typeinfo>
 
 using namespace yack;
 
 namespace
 {
 
-
-
-    /* This function takes last element as pivot, places
-     the pivot element at its correct position in sorted
-     array, and places all smaller (smaller than pivot)
-     to left of pivot and all greater elements to right
-     of pivot */
-    int partition (int arr[], int low, int high)
+    template <typename T>
+    size_t partition( T arr[], const size_t lo, const size_t hi)
     {
-        int pivot = arr[high]; // pivot
-        int i = (low - 1); // Index of smaller element and indicates the right position of pivot found so far
 
-        for (int j = low; j <= high - 1; j++)
+#if 0
+        // Left index
+        i := lo - 1
+
+        // Right index
+        j := hi + 1
+
+        loop forever
+        // Move the left index to the right at least once and while the element at
+        // the left index is less than the pivot
+        do i := i + 1 while A[i] < pivot
+
+        // Move the right index to the left at least once and while the element at
+        // the right index is greater than the pivot
+        do j := j - 1 while A[j] > pivot
+
+        // If the indices crossed, return
+        if i ≥ j then return j
+
+        // Swap the elements at the left and right indices
+        swap A[i] with A[j]
+#endif
+        const T pivot = arr[ (lo+hi)>>1 ];
+        size_t  i     = lo-1;
+        size_t  j     = hi+1;
+        while(true)
         {
-            // If current element is smaller than the pivot
-            if (arr[j] < pivot)
+            do ++i; while ( arr[i]<pivot );
+            do --j; while ( arr[j]>pivot );
+            if(i>=j) return j;
+            cswap(arr[i],arr[j]);
+        }
+    }
+
+    template <typename T>
+    void quickSortAlgo( T arr[], const size_t lo, const size_t hi)
+    {
+        assert(lo>0);
+        if(lo<hi)
+        {
+            const size_t p = partition(arr,lo,hi);
+            quickSortAlgo(arr,lo,p);
+            quickSortAlgo(arr,p+1,hi);
+        }
+    }
+
+    template <typename T>
+    void quickSort(contiguous<T> &arr)
+    {
+        quickSortAlgo(*arr,1,arr.size());
+    }
+
+    template <typename T>
+    static inline void testQS(randomized::bits &ran)
+    {
+        std::cerr << "testQS<" << typeid(T).name() << ">" << std::endl;
+        for(size_t n=0;n<=512;++n)
+        {
+            vector<T> data(n,0);
+            for(size_t iter=0;iter<64;++iter)
             {
-                i++; // increment index of smaller element
-                cswap(arr[i],arr[j]);
+                bring::fill(data,ran);
+                //std::cerr << "raw=" << data << std::endl;
+                quickSort(data);
+                //std::cerr << "srt=" << data << std::endl;
+                YACK_ASSERT(comparison::ordered(data,comparison::increasing<T>));
             }
         }
-        cswap(arr[i+1],arr[high]);
-        return (i + 1);
-    }
-
-
-
-    /* The main function that implements QuickSort
-     arr[] --> Array to be sorted,
-     low --> Starting index,
-     high --> Ending index */
-    void quickSort(int arr[], int low, int high)
-    {
-        if (low < high)
-        {
-            /* pi is partitioning index, arr[p] is now
-             at right place */
-            int pi = partition(arr, low, high);
-
-            // Separately sort elements before
-            // partition and after partition
-            quickSort(arr, low, pi - 1);
-            quickSort(arr, pi + 1, high);
-        }
-    }
-
-    /* Function to print an array */
-    void printArray(int arr[], int size)
-    {
-        int i;
-        for (i = 0; i < size; i++)
-            std::cout << arr[i] << " ";
-        std::cout << std::endl;
     }
 
 }
@@ -67,14 +91,11 @@ namespace
 
 YACK_UTEST(sort_quick)
 {
-    int       arr[] = {10, 7, 8, 9, 1, 5};
-    const int n = sizeof(arr) / sizeof(arr[0]);
-    std::cout << "Raw Array: \n";
-    printArray(arr,n);
-    quickSort(arr, 0, n - 1);
-    std::cout << "Sorted array: \n";
-    printArray(arr, n);
-    return 0;
+    randomized::rand_ ran;
+
+    testQS<int>(ran);
+    testQS<double>(ran);
+
 }
 YACK_UDONE()
 
