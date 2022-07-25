091f5dce410e61cbec1167da1ec5aaabe026b590
Yann Bouret <yann.bouret@unice.fr>
2022-07-25 09:48:49 +0200

    wip

--- src/core/tests/sort/quick.cpp
+++ src/core/tests/sort/quick.cpp
@@ -13,29 +13,7 @@ namespace
     template <typename T>
     size_t partition( T arr[], const size_t lo, const size_t hi)
     {
-
-#if 0
-        // Left index
-        i := lo - 1
-
-        // Right index
-        j := hi + 1
-
-        loop forever
-        // Move the left index to the right at least once and while the element at
-        // the left index is less than the pivot
-        do i := i + 1 while A[i] < pivot
-
-        // Move the right index to the left at least once and while the element at
-        // the right index is greater than the pivot
-        do j := j - 1 while A[j] > pivot
-
-        // If the indices crossed, return
-        if i ≥ j then return j
-
-        // Swap the elements at the left and right indices
-        swap A[i] with A[j]
-#endif
+        
         const T pivot = arr[ (lo+hi)>>1 ];
         size_t  i     = lo-1;
         size_t  j     = hi+1;
