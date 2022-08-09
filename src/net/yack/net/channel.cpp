cb452d1d48eed537374a0edfcccec0d3de93cac3
Yann Bouret <yann.bouret@unice.fr>
2022-08-09 08:53:47 +0200

    wip

---
+++ src/net/yack/net/channel.cpp
@@ -0,0 +1,13 @@
+
+#include "yack/net/channel.hpp"
+
+namespace yack
+{
+    namespace net
+    {
+        channel::  channel() throw() {}
+        channel:: ~channel() throw() {}
+    }
+}
+
+
---
+++ src/net/yack/net/channel.hpp
@@ -0,0 +1,28 @@
+
+//! \file
+
+#ifndef YACK_NET_CHANNEL_INCLUDED
+#define YACK_NET_CHANNEL_INCLUDED 1
+
+#include "yack/net/socket/address.hpp"
+
+namespace yack
+{
+    namespace net
+    {
+        class channel
+        {
+        public:
+            virtual ~channel() throw();
+
+        protected:
+            explicit channel() throw();
+            
+        private:
+            YACK_DISABLE_COPY_AND_ASSIGN(channel);
+        };
+    }
+}
+
+#endif
+
