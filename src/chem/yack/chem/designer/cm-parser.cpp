1223bb6b36ac177fa75311b5882222104c311b69
Yann Bouret <yann.bouret@unice.fr>
2022-08-11 11:40:22 +0200

    wip

--- src/chem/yack/chem/designer/cm-parser.cpp
+++ src/chem/yack/chem/designer/cm-parser.cpp
@@ -14,32 +14,25 @@ namespace yack
         {
 
             cm_parser:: ~cm_parser() throw() {}
-
-
-
+            
             cm_parser:: cm_parser() : jive:: parser("chemical::components::parser")
             {
-#if 0
-                compound   &COMPONENTS = agg("COMPONENTS");
-                compound   &COMPONENT  = ( agg("COMPONENT") << mark('[') << oom( term("CH",ch_expr) ) << mark(']') );
-                const rule &COEFF      = term("COEFF","[1-9][0-9]*");
-                
-                COMPONENTS << COMPONENT;
 
-                COMPONENTS << zom( COMPONENT );
-#endif
-                
                 compound   &COMPONENTS = agg("COMPONENTS");
-                compound   &COMPONENT = agg("COMPONENT");
-                const rule &POS       = term('+');
-                const rule &NEG       = term('-');
-                const rule &XPOS      = oom(POS);
-                const rule &XNEG      = oom(NEG);
+                compound   &COMPONENT  = act("COMPONENT");
+                const rule &POS        = term('+');
+                const rule &NEG        = term('-');
+                const rule &SGN        = choice(POS,NEG);
+                const rule &XPOS       = oom(POS);
+                const rule &XNEG       = oom(NEG);
                 COMPONENT << mark('[') << ( agg(COM::SPECIES) << term(COM::SP_NAME,COM::SP_EXPR) << opt( choice(XPOS,XNEG) ) ) << mark(']');
 
-                COMPONENTS << COMPONENT;
-
-                COMPONENTS << zom(COMPONENT);
+                const rule &COEF       = term(COM::COEF,"[1-9][0-9]*");
+                const rule &FIRST_COEF = (agg(COM::FIRST_COEF) << choice(cat(SGN,COEF),SGN,COEF));
+                const rule &EXTRA_COEF = (agg(COM::EXTRA_COEF) << cat(SGN,opt(COEF)));
+                
+                COMPONENTS << opt(FIRST_COEF) << COMPONENT;
+                COMPONENTS << zom( cat(EXTRA_COEF,COMPONENT) );
 
                 drop("[:blank:]+");
 
--- src/chem/yack/chem/designer/com.hpp
+++ src/chem/yack/chem/designer/com.hpp
@@ -16,9 +16,13 @@ namespace yack
 
             struct COM
             {
-                static const char SPECIES[];  //!< "SPECIES"
-                static const char SP_NAME[];  //!< "SP_NAME"
-                static const char SP_EXPR[];  //!< regular expression
+                static const char SPECIES[];     //!< "SPECIES"
+                static const char SP_NAME[];     //!< "SP_NAME"
+                static const char SP_EXPR[];     //!< regular expression
+                static const char COEF[];       //!< "COEF"
+                static const char FIRST_COEF[]; //!< "FIRST_COEF"
+                static const char EXTRA_COEF[]; //!< "EXTRA_COEF"
+
             };
 
         }
