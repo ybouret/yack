ebd0bad4e2f50f5ed0357feb7dbedfc151a4e9a3
Yann Bouret <yann.bouret@unice.fr>
2022-08-12 18:26:31 +0200

    wip

--- src/chem/yack/chem/outcome.cpp
+++ src/chem/yack/chem/outcome.cpp
@@ -217,29 +217,116 @@ namespace yack
             // find best Cend + nu * xi
             {
                 double width = fabs(x.c-x.a); assert(width>0);
-
+                bool   flag  = true;
             ZFIND:
                 //--------------------------------------------------------------
-                // middle point
+                //
+                // take middle point
+                //
                 //--------------------------------------------------------------
                 assert(f.a>0);
                 assert(f.c<0);
                 f.b = F( x.b = clamp(x.a, 0.5*(x.a+x.c), x.c) ); assert(x.is_increasing());
+                const double den = fabs( squared(f.b) - f.a*f.c );
 
                 switch( __sign::of(f.b) )
                 {
+                        //------------------------------------------------------
+                        //
+                        // early return
+                        //
+                        //------------------------------------------------------
                     case __zero__:
                         comp.move(Cend,x.b);
                         goto SUCCESS;
 
+                        //------------------------------------------------------
+                        //
+                        // hybrid step with f.b < 0
+                        //
+                        //------------------------------------------------------
                     case negative: {
-                        f.c = f.b;
-                        x.c = x.b;
+                        if( flag && fabs(f.b)<den)
+                        {
+                            //--------------------------------------------------
+                            // Ridder's step in x.a:x.b
+                            //--------------------------------------------------
+                            const double x_r = x.b+0.5*( width * (f.b/den) );
+                            const double x_u = clamp(x.a,x_r, x.b );
+                            const double f_u = F(x_u);
+
+                            switch( __sign::of(f_u) )
+                            {
+                                case __zero__:
+                                    comp.move(Cend,x_u);
+                                    goto SUCCESS;
+
+                                case negative:
+                                    f.c = f.b = f_u;
+                                    x.c = x.b = x_u;
+                                    break;
+
+                                case positive: assert(f.b<0);
+                                    f.c = f.b;
+                                    x.c = x.b;
+                                    f.a = (f.b = f_u);
+                                    x.a = (x.b = x_u);
+                                    break;
+                            }
+
+                        }
+                        else
+                        {
+                            //--------------------------------------------------
+                            // bisection
+                            //--------------------------------------------------
+                            f.c = f.b;
+                            x.c = x.b;
+                        }
                     } break;
 
+                        //------------------------------------------------------
+                        //
+                        // hybrid step with f.b > 0
+                        //
+                        //------------------------------------------------------
                     case positive: {
-                        f.a = f.b;
-                        x.a = x.b;
+                        if(flag && fabs(f.b)<den)
+                        {
+                            //--------------------------------------------------
+                            // Ridder's step in x.b:x.c
+                            //--------------------------------------------------
+                            const double x_r = x.b+0.5*( width * (f.b/den) );
+                            const double x_u = clamp(x.b,x_r, x.c );
+                            const double f_u = F(x_u);
+
+                            switch( __sign::of(f_u) )
+                            {
+                                case __zero__:
+                                    comp.move(Cend,x_u);
+                                    goto SUCCESS;
+
+                                case negative: assert(f.b>0);
+                                    f.a = f.b;
+                                    x.a = x.b;
+                                    f.c = (f.b = f_u);
+                                    x.c = (x.b = x_u);
+                                    break;
+
+                                case positive:
+                                    f.a = f.b = f_u;
+                                    x.a = x.b = x_u;
+                                    break;
+                            }
+                        }
+                        else
+                        {
+                            //--------------------------------------------------
+                            // bisection
+                            //--------------------------------------------------
+                            f.a = f.b;
+                            x.a = x.b;
+                        }
                     } break;
                 }
 
