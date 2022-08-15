1d1069973d6fffab9ed718b8e6e9d03b96f9cff3
ybouret <yann.bouret@gmail.com>
2022-08-15 22:25:34 +0200

    wip pair

--- src/core/tests/data/pair.cpp
+++ src/core/tests/data/pair.cpp
@@ -7,10 +7,21 @@ YACK_PAIR_DECL(Pair,int,a,int,b);
 
 YACK_PAIR_END(Pair);
 
- 
+template <typename A> YACK_PAIR_DECL(TPair,A,a,const int,b);
+
+YACK_PAIR_END(TPair);
 
 YACK_UTEST(data_pair)
 {
+    Pair p1(0,1);
+    Pair p2 = p1;
+    std::cerr << "p1=" << p1 << std::endl;
+    std::cerr << "p2=" << p2 << std::endl;
+
+    TPair<float> tp1(1,3);
+    std::cerr << "tp1=" << tp1 << std::endl;
+
+    
 }
 YACK_UDONE()
 
--- src/core/yack/container/pair.hpp
+++ src/core/yack/container/pair.hpp
@@ -4,15 +4,44 @@
 #define YACK_PAIR_INCLUDED 1
 
 #include "yack/type/args.hpp"
+#include <iostream>
 
 namespace yack
 {
+    //__________________________________________________________________________
+    //
+    //
+    //! prolog to create a pair class
+    //
+    //__________________________________________________________________________
 #define YACK_PAIR_DECL(CLASS,TYPE1,NAME1,TYPE2,NAME2) \
-class CLASS { \
-public:\
-
+/**/    class CLASS { \
+/**/      public:\
+/**/        YACK_DECL_ARGS(TYPE1,type1);\
+/**/        YACK_DECL_ARGS(TYPE2,type2);\
+/**/        inline explicit CLASS(param_type1 arg1, param_type2 arg2) :\
+/**/        NAME1(arg1), NAME2(arg2) {}\
+/**/        inline ~CLASS() throw()  {}\
+/**/        inline CLASS(const CLASS &other) : NAME1(other.NAME1), NAME2(other.NAME2) {}\
+/**/        inline static const char *__##NAME1() throw() { return #NAME1; }\
+/**/        inline static const char *__##NAME2() throw() { return #NAME2; }\
+/**/        inline friend std::ostream &operator<<(std::ostream &os, const CLASS &self) {\
+/**/            os << '(';\
+/**/            os << __##NAME1() << '=' << self.NAME1;\
+/**/            os << ',' << __##NAME2() << '=' << self.NAME2;\
+/**/            os << ')';\
+/**/        return os; } \
+/**/        TYPE1 NAME1; \
+/**/        TYPE2 NAME2
+    
+    //__________________________________________________________________________
+    //
+    //
+    //! epilog to create a pair class
+    //
+    //__________________________________________________________________________
 #define YACK_PAIR_END(CLASS) \
-private: YACK_DISABLE_ASSIGN(CLASS); }
+/**/ private: YACK_DISABLE_ASSIGN(CLASS); }
 
 }
 
