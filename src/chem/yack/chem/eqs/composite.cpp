6c1dc96883e1fe8eec58dced842629dadfb2f3fd
Yann Bouret <yann.bouret@unice.fr>
2022-08-16 09:38:39 +0200

    wip

--- src/chem/yack/chem/eqs/composite.cpp
+++ src/chem/yack/chem/eqs/composite.cpp
@@ -20,27 +20,33 @@ namespace yack
                                    const components &rhs)
         {
 
+            // initialize
             cof.free();
 
+            // loop over LHS
             for(const cnode *lnode=lhs.head();lnode;lnode=lnode->next)
             {
                 const component &lcomp  = ***lnode;
                 const size_t     lindx  =  **lcomp;
                 const int        lcoef  = lcomp.nu;
 
+                // loop over RHS
                 for(const cnode *rnode=rhs.head();rnode;rnode=rnode->next)
                 {
                     const component &rcomp  = ***rnode;
                     const size_t     rindx  =  **rcomp;
 
+                    // not the same
                     if(rindx!=lindx) continue;
 
                     std::cerr << "\t\tsharing " << (*rcomp).name << std::endl;
 
-                    bool  found = false;
+                    // guess coefficient
                     coeff guess(lcoef,rcomp.nu);
                     guess.normalize();
 
+                    // check existing
+                    bool  found = false;
                     for(size_t i=cof.size();i>0;--i)
                     {
                         if(guess == cof[i]) { found=true; break; }
