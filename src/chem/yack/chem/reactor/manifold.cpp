5537a5eea71dbfb3a77dc1269291716f5f7299e4
Yann Bouret <yann.bouret@unice.fr>
2022-08-17 15:59:56 +0200

    need to reduce complexity

--- src/chem/yack/chem/reactor/manifold.cpp
+++ src/chem/yack/chem/reactor/manifold.cpp
@@ -16,27 +16,31 @@ namespace yack
             const group   __empty;
             vector<group> detached(L,__empty); assert(L==detached.size());
 
-            for(const enode *lhs = lattice.head(); lhs; lhs=lhs->next)
+            for(const group *source = related.head; source; source=source->next)
             {
-                const equilibrium &LHS = ***lhs;
-                const size_t       LID = *LHS;
-                group             &LGP = detached[LID];
-
-                for(const enode *rhs = lhs->next;rhs;rhs=rhs->next)
+                for(size_t i=L;i>0;--i) detached[i].release();
+                for(const gnode *lhs=source->head;lhs;lhs=lhs->next)
                 {
-                    const equilibrium &RHS = ***rhs;
-                    const size_t       RID = *RHS;
+                    const equilibrium &LHS = **lhs;
+                    const size_t       LID = *LHS;
+                    group             &LGP = detached[LID];
 
-                    if( LHS.detached_of(RHS) )
+                    for(const gnode *rhs = lhs->next;rhs;rhs=rhs->next)
                     {
-                        LGP           << &RHS;
-                        detached[RID] << &LHS;
+                        const equilibrium &RHS = **rhs;
+                        const size_t       RID = *RHS;
+
+                        if( LHS.detached_of(RHS) )
+                        {
+                            LGP           << &RHS;
+                            detached[RID] << &LHS;
+                        }
                     }
                 }
+                lattice(std::cerr,"",detached);
             }
+            
 
-            lattice(std::cerr,"",detached);
-            //std::cerr << detached << std::endl;
 
 
             
