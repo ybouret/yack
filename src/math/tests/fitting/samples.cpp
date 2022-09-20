7c0029403487ad7d76c5f85fbe92faa1c1bd910d
Yann Bouret <yann.bouret@unice.fr>
2022-09-20 14:19:50 +0200

    cleanup

--- src/math/tests/fitting/samples.cpp
+++ src/math/tests/fitting/samples.cpp
@@ -1,4 +1,5 @@
 #include "yack/math/fitting/samples.hpp"
+#include "yack/math/fitting/least-squares.hpp"
 #include "yack/type/v2d.hpp"
 #include "yack/utest/run.hpp"
 
@@ -10,6 +11,23 @@ typedef v2d<double> vtx;
 
 namespace
 {
+
+    struct Circle
+    {
+
+        inline
+        double operator()(const vtx                &p,
+                          const readable<double>   &aorg,
+                          const fitting::variables &vars)
+        {
+            const double rc = vars(aorg,"rc");
+            const double xc = vars(aorg,"xc");
+            const double yc = vars(aorg,"yc");
+            return rc*rc  + 2 *(xc*p.x+yc*p.y) - (xc*xc+yc*yc);
+        }
+
+    };
+
     static inline void generate(vector<vtx>    &P,
                                 vector<double> &Z,
                                 const size_t    N,
@@ -22,7 +40,7 @@ namespace
             const double radius = r * (1.0 + 0.1 * ran.symm<double>());
             const double theta  = ((6.3 * i) / N) * (1.0 + 0.1 * ran.symm<double>());
             P[i] = C + radius * vtx( cos(theta), sin(theta) );
-            Z[i] = 0;
+            Z[i] = P[i].norm2();
         }
     }
 
@@ -34,6 +52,26 @@ namespace
         {
             fp("%g %g\n",s.abscissa[i].x,s.abscissa[i].y);
         }
+    }
+
+    template <typename SAMPLE>
+    static inline void save_fit(const SAMPLE &s,
+                                const readable<double>   &aorg,
+                                const fitting::variables &vars)
+    {
+        const double rc = vars(aorg,"rc");
+        const double xc = vars(aorg,"xc");
+        const double yc = vars(aorg,"yc");
+
+        ios::ocstream fp( s.name + ".fit" );
+        const size_t np = 100;
+        for(size_t i=0;i<=np;++i)
+        {
+            const double theta = (6.3 * i) / np;
+            const vtx p = vtx(xc,yc) + rc * vtx( cos(theta), sin(theta) );
+            fp("%g %g\n", p.x, p.y);
+        }
+
     }
 
 }
@@ -60,15 +98,72 @@ YACK_UTEST(fitting_samples)
     std::cerr << circle1.name << " #" << circle1.dimension() << std::endl;
     std::cerr << circle2.name << " #" << circle2.dimension() << std::endl;
 
-    const double r1 = 1.4;
-    const double r2 = 2.1;
+    const double r = 1.4;
 
     const vtx    C1(1,1);
     const vtx    C2(2,-2);
 
-    generate(P1,Z1,N1,r1,C1,ran); save_sample(circle1);
-    generate(P2,Z2,N2,r2,C2,ran); save_sample(circle2);
+    fitting::variables &vars = *circles;
+    vars << "r:x1:y1:x2:y2";
+    std::cerr << "vars = " << vars << std::endl;
 
+    fitting::variables &v1 = *circle1;
+    v1("rc",vars,"r");
+    v1("xc",vars,"x1");
+    v1("yc",vars,"y1");
+    std::cerr << "v1   = " << v1 << std::endl;
+
+    fitting::variables &v2 = *circle2;
+    v2("rc",vars,"r");
+    v2("xc",vars,"x2");
+    v2("yc",vars,"y2");
+    std::cerr << "v2   = " << v2 << std::endl;
+
+
+    const size_t   nvar = vars.upper();
+    vector<double> aorg(nvar,0);
+    vector<bool>   used(nvar,true);
+    vector<double> aerr(nvar,0);
+    vector<double> scal(nvar,1e-4);
+
+    generate(P1,Z1,N1,r,C1,ran); save_sample(circle1);
+    generate(P2,Z2,N2,r,C2,ran); save_sample(circle2);
+
+    fitting::least_squares<vtx,double> ls;
+    ls.verbose = true;
+
+    double &radius = vars(aorg,"r");
+    double &x1     = vars(aorg,"x1");
+    double &y1     = vars(aorg,"y1");
+
+    radius = 1;
+    x1     = 0.8;
+    y1     = 1.1;
+
+    Circle F;
+
+    v1(used,"rc") = false;
+    ls.fit_with(F,circle1,aorg,used,scal,aerr);
+
+    v1(used,"rc") = true;
+    ls.fit_with(F,circle1,aorg,used,scal,aerr);
+
+    save_fit(circle1,aorg,v1);
+
+    double &x2 = vars(aorg,"x2");
+    double &y2 = vars(aorg,"y2");
+
+    x2 =  1.5;
+    y2 = -2.8;
+    v1(used,"rc") = false;
+    ls.fit_with(F,circle2,aorg,used,scal,aerr);
+    save_fit(circle2,aorg,v2);
+
+    v1(used,"rc") = true;
+    ls.fit_with(F,circles,aorg,used,scal,aerr);
+
+    save_fit(circle1,aorg,v1);
+    save_fit(circle2,aorg,v2);
 
 
 
