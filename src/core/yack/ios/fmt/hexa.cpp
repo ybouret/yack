b273944f5b40a5b03ee988c8d7c687491fe69ee9
Yann Bouret <yann.bouret@unice.fr>
2022-09-30 15:33:24 +0200

    wip

--- src/core/yack/ios/fmt/hexa.cpp
+++ src/core/yack/ios/fmt/hexa.cpp
@@ -67,7 +67,7 @@ namespace yack
 
         ios::ostream & operator<<(ios::ostream &os, const hexa &x)
         {
-            char tab[16];
+            char tab[16] = { 0 };
             //size_t n = x.load(tab); assert(n>0);
             os.frame(tab,x.load(tab));
             return os;
