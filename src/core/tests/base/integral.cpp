4f3d6efa3f1dbe44689a9050a90c631cce40f3ba
Yann Bouret <yann.bouret@unice.fr>
2022-11-04 10:58:12 +0100

    wip

--- src/core/tests/base/integral.cpp
+++ src/core/tests/base/integral.cpp
@@ -5,13 +5,17 @@ using namespace yack;
 
 YACK_UTEST(integral)
 {
-#define VALUE 17
+#define VALUE 127
 
     YACK_CHECK(VALUE == (integral::convert<uint8_t,uint8_t>(VALUE,NULL)) );
     YACK_CHECK(VALUE == (integral::convert<uint8_t,uint16_t>(VALUE,NULL)) );
     YACK_CHECK(VALUE == (integral::convert<uint8_t,uint32_t>(VALUE,NULL)) );
     YACK_CHECK(VALUE == (integral::convert<uint8_t,uint64_t>(VALUE,NULL)) );
 
+    YACK_CHECK(VALUE == (integral::convert<uint8_t,int8_t>(VALUE,NULL)) );
+    YACK_CHECK(VALUE == (integral::convert<uint8_t,int16_t>(VALUE,NULL)) );
+    YACK_CHECK(VALUE == (integral::convert<uint8_t,int32_t>(VALUE,NULL)) );
+    YACK_CHECK(VALUE == (integral::convert<uint8_t,int64_t>(VALUE,NULL)) );
 
 }
 YACK_UDONE()
--- src/core/yack/type/integral.cpp
+++ src/core/yack/type/integral.cpp
@@ -2,12 +2,15 @@
 #include "yack/system/exception.hpp"
 #include <cerrno>
 
+#define CTX (ctx?ctx:yack_unknown)
+
+
 namespace yack
 {
-#define CTX (ctx?ctx:yack_unknown)
+
     //--------------------------------------------------------------------------
     //
-    // TARGET=uint8_t
+    // TARGET=uint8_t, SOURCE=UNSIGNED
     //
     //--------------------------------------------------------------------------
     template <> uint8_t integral:: convert<uint8_t,uint8_t>(const uint8_t source, const char *)
@@ -17,20 +20,57 @@ namespace yack
 
     template <> uint8_t integral:: convert<uint8_t,uint16_t>(const uint16_t source, const char *ctx)
     {
-        if(source>=255) throw libc::exception(EDOM,"uint8_t <- uint16_t %s",CTX);
+        if(source>255) throw libc::exception(EDOM,"uint8_t <- uint16_t %s",CTX);
         return uint8_t(source);
     }
 
     template <> uint8_t integral:: convert<uint8_t,uint32_t>(const uint32_t source, const char *ctx)
     {
-        if(source>=255) throw libc::exception(EDOM,"uint8_t <- uint32_t %s",CTX);
+        if(source>255) throw libc::exception(EDOM,"uint8_t <- uint32_t %s",CTX);
         return uint8_t(source);
     }
 
     template <> uint8_t integral:: convert<uint8_t,uint64_t>(const uint64_t source, const char *ctx)
     {
-        if(source>=255) throw libc::exception(EDOM,"uint8_t <- uint64_t %s",CTX);
+        if(source>255) throw libc::exception(EDOM,"uint8_t <- uint64_t %s",CTX);
         return uint8_t(source);
     }
 
+
+}
+
+
+
+namespace yack
+{
+
+    //--------------------------------------------------------------------------
+    //
+    // TARGET=uint8_t, SOURCE=SIGNED
+    //
+    //--------------------------------------------------------------------------
+    template <> uint8_t integral:: convert<uint8_t,int8_t>(const int8_t source, const char *ctx)
+    {
+        if(source<0) throw libc::exception(EDOM,"uint8_t <- int8_t %s",CTX);
+        return uint8_t(source);
+    }
+
+    template <> uint8_t integral:: convert<uint8_t,int16_t>(const int16_t source, const char *ctx)
+    {
+        if(source<0 || source>255) throw libc::exception(EDOM,"uint8_t <- int16_t %s",CTX);
+        return uint8_t(source);
+    }
+
+    template <> uint8_t integral:: convert<uint8_t,int32_t>(const int32_t source, const char *ctx)
+    {
+        if(source<0 || source>255) throw libc::exception(EDOM,"uint8_t <- int32_t %s",CTX);
+        return uint8_t(source);
+    }
+
+    template <> uint8_t integral:: convert<uint8_t,int64_t>(const int64_t source, const char *ctx)
+    {
+        if(source<0 || source>255) throw libc::exception(EDOM,"uint8_t <- int64_t %s",CTX);
+        return uint8_t(source);
+    }
+    
 }
