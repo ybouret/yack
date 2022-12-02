105865cd344494d4ce7fdef50a09b258e093b69c
Yann Bouret <yann.bouret@unice.fr>
2022-12-02 12:15:25 +0100

    basic bunch

--- src/core/tests/data/bunch.cpp
+++ src/core/tests/data/bunch.cpp
@@ -1,12 +1,24 @@
 #include "yack/sequence/bunch.hpp"
 #include "yack/utest/run.hpp"
+#include "../main.hpp"
+#include "yack/sequence/vector.hpp"
 
 using namespace yack;
 
 YACK_UTEST(data_bunch)
 {
-    bunch<int> ib(6);
-    
+    randomized::rand_ ran;
+
+    bunch<int>  ib(6);
+    vector<int> toto(6);
+    for(size_t i=1;i<=8;++i)
+    {
+        bring::fill(toto,ran);
+        ib.ensure(toto);
+        std::cerr << "ib=" << *ib << std::endl;
+    }
+
+
 }
 YACK_UDONE()
 
--- src/core/yack/sequence/bunch.hpp
+++ src/core/yack/sequence/bunch.hpp
@@ -32,7 +32,6 @@ namespace yack
         typedef cxx_list_of<entry> entries;
 
 
-
         inline explicit bunch(const size_t w) throw() :
         width(w)
         {
@@ -45,12 +44,58 @@ namespace yack
             while(n-- > 0) zpool.push_back( new entry(width) );
         }
 
-        inline void free()
+        inline void free() throw()
         {
             zpool.merge_front(alive);
         }
 
+        inline void release()  throw()
+        {
+            alive.release();
+            zpool.release();
+        }
+
+        const entries * operator->() const throw() { return &alive; }
+        const entries & operator*()  const throw() { return  alive; }
+
+        template <typename ARRAY> inline
+        bool search(ARRAY &arr) const
+        {
+            assert(width==arr.size());
+            for(const entry *ep=alive.head;ep;ep=ep->next)
+            {
+                if( are_same(arr,*ep) ) return true;
+            }
+            return false;
+        }
+
+        template <typename ARRAY> inline
+        bool insert(ARRAY &arr)
+        {
+            if(search(arr))
+                return false;
+            else
+            {
+                entry &tgt = * alive.push_back( query() );
+                try {
+                    for(size_t i=width;i>0;--i) tgt[i] = arr[i];
+                }
+                catch(...)
+                {
+                    zpool.push_back( alive.pop_back() );
+                }
+                return true;
+            }
+        }
+
+        template <typename ARRAY> inline
+        void ensure(ARRAY &arr) {
+            (void)insert(arr);
+        }
+
+
         const size_t width;
+
 
 
 
@@ -58,6 +103,23 @@ namespace yack
         YACK_DISABLE_COPY_AND_ASSIGN(bunch);
         entries alive; //!< in use
         entries zpool; //!< unsued
+
+        inline entry  *query()
+        {
+            return zpool.size > 0 ? zpool.pop_back() : new entry(width);
+        }
+
+        template <typename ENTRY> inline
+        bool are_same(ENTRY &lhs, const entry &rhs) const
+        {
+            assert(lhs.size()==width);
+            assert(rhs.size()==width);
+            for(size_t i=width;i>0;--i)
+            {
+                if(lhs[i]!=rhs[i]) return false;
+            }
+            return true;
+        }
     };
 
 }
