0722b7c23135be0d15f7546175e75f97542f5071
Yann Bouret <yann.bouret@unice.fr>
2022-12-15 08:36:54 +0100

    definite apz

--- src/core/tests/main.cpp
+++ src/core/tests/main.cpp
@@ -123,6 +123,7 @@ YACK_UTEST_DECL(256)
     YACK_UTEST(apex_mem);
     YACK_UTEST(apn);
     YACK_UTEST(apz);
+    YACK_UTEST(apz_definite);
     YACK_UTEST(apq);
     YACK_UTEST(apex_ari);
     YACK_UTEST(apex_io);
--- src/core/yack/apex/kernel/z.cpp
+++ src/core/yack/apex/kernel/z.cpp
@@ -57,56 +57,106 @@ namespace yack
     
     void apk:: definite(writable<apz> &z, apn &z2)
     {
-        z2.ldz();
+        //----------------------------------------------------------------------
+        //
+        // find first positive value in g, keep track of signs
+        //
+        //----------------------------------------------------------------------
         const size_t n = z.size();
+        z2.ldz();
         switch(n)
         {
             case 0: return;
             case 1:
                 switch(z[1].s)
                 {
-                    case __zero__:          break;
+                    case __zero__: break;
                     case positive:
-                    case negative:
-                        z[1]=1;
-                        z2 = 1; break;
+                    case negative: z[1].ldi(1); z2.ldu(1); break;
                 }
                 return;
             default:
                 break;
         }
         
+
         //----------------------------------------------------------------------
-        // find first positive value
+        //
+        // compute metrics
+        //
         //----------------------------------------------------------------------
-        apn          g = 1;
-        size_t       j = 1;
-        for(;j<=n;++j)
+        sign_type    f = __zero__; // fist sign
+        size_t       m = 0;        // count of minus signs
+        size_t       p = 0;        // count of positive signs
+        apn          g = 1;        // current gcd
         {
-            const apz &q = z[j];
-            if(__zero__ != q.s) {
-                g = q.n;
+            //------------------------------------------------------------------
+            // find first divider and its sign
+            //------------------------------------------------------------------
+            size_t       j = 1;
+            for(;j<=n;++j)
+            {
+                const apz &y = z[j];
+                switch(y.s)
+                {
+                    case __zero__: continue;
+                    case positive: p=1; f = positive; break;
+                    case negative: m=1; f = negative; break;
+                }
+                g = y.n;
                 break;
             }
-        }
-        
-        //----------------------------------------------------------------------
-        // compute with other positive values
-        //----------------------------------------------------------------------
-        for(++j;j<=n;++j)
-        {
-            const apz &q = z[j];
-            if(__zero__ != q.s) {
-                g = apn::gcd(q.n,g);
+            assert(p+m<=1);
+
+            //------------------------------------------------------------------
+            // find first divider, keep track of signs
+            //------------------------------------------------------------------
+            for(++j;j<=n;++j)
+            {
+                const apz &y = z[j];
+                switch(y.s)
+                {
+                    case __zero__: continue;
+                    case positive: ++p; break;
+                    case negative: ++m;  break;
+                }
+                g = apn::gcd(y.n,g);
             }
         }
-        
+
         //----------------------------------------------------------------------
+        //
         // simplify
+        //
         //----------------------------------------------------------------------
-        for(size_t i=n;i>0;--i)
+        if( (m>p) || ( (m==p) && negative==f) )
         {
-            z2 += apn::squared( (z[i] /= g).n );
+            for(size_t i=n;i>0;--i)
+            {
+                apz &y = z[i];
+                switch(y.s)
+                {
+                    case __zero__: continue;
+                    case positive: coerce(y.s) = negative; break;
+                    case negative: coerce(y.s) = positive; break;
+                }
+                z2 += apn::squared( coerce(y.n) /= g );
+            }
+        }
+        else
+        {
+            for(size_t i=n;i>0;--i)
+            {
+                apz &y = z[i];
+                switch(y.s)
+                {
+                    case __zero__: continue;
+                    case positive:
+                    case negative:
+                        z2 += apn::squared( coerce(y.n) /= g );
+                        continue;
+                }
+            }
         }
     }
     
