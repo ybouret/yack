fc6e8471853f70bf613d4af52e20e1a460828504
Yann Bouret <yann.bouret@unice.fr>
2022-12-15 14:24:54 +0100

    initi

--- src/core/tests/raven/family.cpp
+++ src/core/tests/raven/family.cpp
@@ -1,5 +1,5 @@
 
-#include "yack/raven/qmatrix.hpp"
+#include "yack/raven/qfamily.hpp"
 #include "yack/raven/qselect.hpp"
 #include "yack/utest/run.hpp"
 
@@ -15,11 +15,44 @@ using namespace yack;
 
 namespace
 {
+    static inline void create(writable<int> &cf, randomized::bits &ran)
+    {
+        for(size_t i=cf.size();i>0;--i)
+        {
+            cf[i] = static_cast<int>( ran.in(-4,4) );
+        }
+    }
+
+    static inline void create(matrix<int> &nu, randomized::bits &ran)
+    {
+        for(size_t i=nu.rows;i>0;--i)
+        {
+            create(nu[i],ran);
+        }
+    }
+
 }
 
 
 YACK_UTEST(raven_family)
 {
+    randomized::rand_ ran;
+
+    {
+        const size_t eqs = 2;
+        const size_t spc = 3;
+
+        matrix<int> nu(eqs,spc);
+        create(nu,ran);
+        matrix<int> mu;
+        raven::qselect::compress(mu,nu);
+
+        std::cerr << "nu=" << nu << std::endl;
+        std::cerr << "mu=" << mu << std::endl;
+
+
+    }
+
 
 }
 YACK_UDONE()
