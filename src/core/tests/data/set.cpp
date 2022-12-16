b12082f0698fc749f03f63f8da63734a600e3db4
Yann Bouret <yann.bouret@unice.fr>
2022-12-16 10:17:12 +0100

    need the look up procedure in data_set

--- src/core/tests/data/set.cpp
+++ src/core/tests/data/set.cpp
@@ -7,8 +7,33 @@ using namespace yack;
 YACK_UTEST(data_set)
 {
 
-    ds_zpool<int> izp;
-    izp.reserve(100);
+    ds_zpool<int>::pointer izp = new ds_zpool<int>();
+    izp->reserve(100);
+
+    data_set<int> ids( izp );
+
+    YACK_CHECK(!ids.contains(4));
+
+    YACK_CHECK(ids.insert(12));
+    YACK_CHECK(ids.insert(13));
+    std::cerr << *ids << std::endl;
+    ids.free();
+    YACK_CHECK(ids.insert(13));
+    YACK_CHECK(ids.insert(12));
+    YACK_CHECK(ids.insert(4));
+    YACK_CHECK(ids.insert(15));
+
+    std::cerr << *ids << std::endl;
+    YACK_CHECK(ids.contains(4));
+    YACK_CHECK(ids.contains(15));
+
+
+
+
+    YACK_SIZEOF(ds_zpool<int>);
+    YACK_SIZEOF(ds_zpool<string>);
+    YACK_SIZEOF(data_set<int>);
+    YACK_SIZEOF(data_set<string>);
 
 }
 YACK_UDONE()
--- src/core/yack/data/set.hpp
+++ src/core/yack/data/set.hpp
@@ -10,6 +10,8 @@
 #include "yack/type/args.hpp"
 #include "yack/type/destruct.hpp"
 #include "yack/type/out-of-reach.hpp"
+#include "yack/ptr/arc.hpp"
+#include "yack/signs.hpp"
 
 namespace yack
 {
@@ -22,6 +24,13 @@ namespace yack
         inline  ds_node(const_type &args) : next(0), prev(0), data(args) {}
         inline ~ds_node() throw() {}
 
+        inline const_type & operator*() const throw() { return data; }
+        inline type       & operator*()       throw() { return data; }
+        inline friend std::ostream & operator<<(std::ostream &os, const ds_node &self) {
+            os << self.data;
+            return os;
+        }
+
         ds_node *next;
         ds_node *prev;
     private:
@@ -33,7 +42,9 @@ namespace yack
     class ds_zpool : public object, public counted
     {
     public:
-        typedef ds_node<T> node_type;
+        typedef ds_node<T>        node_type;
+        typedef arc_ptr<ds_zpool> pointer;
+
         YACK_DECL_ARGS_(T,type);
         inline explicit ds_zpool() throw() : object(), counted() {}
         inline virtual ~ds_zpool() throw() { release(); }
@@ -65,10 +76,6 @@ namespace yack
         }
 
 
-
-
-
-
     private:
         YACK_DISABLE_COPY_AND_ASSIGN(ds_zpool);
         pool_of<node_type> repo;
@@ -80,29 +87,118 @@ namespace yack
     {
     public:
         YACK_DECL_ARGS(T,type);
-        class node_type
-        {
-        public:
-            inline  node_type(const_type &args) : next(0), prev(0), data(args) {}
-            inline ~node_type() throw() {}
-
-            node_type *next;
-            node_type *prev;
-        private:
-            YACK_DISABLE_COPY_AND_ASSIGN(node_type);
-            mutable_type data;
-        };
-
+        typedef ds_node<T>         node_type;
         typedef list_of<node_type> list_type;
-        typedef pool_of<node_type> pool_type;
 
-        inline explicit data_set() throw() : alive(), cache() {}
-        inline virtual ~data_set() throw() {}
+        typedef typename ds_zpool<type>::pointer zcache;
 
+        inline explicit data_set(const zcache &shared) throw() :
+        cache(shared)
+        {
+        }
+
+        inline virtual ~data_set() throw()
+        {
+            free();
+        }
+
+        inline void free() throw() {
+            while(items.size) cache->free(items.pop_back());
+        }
+
+
+        bool insert(param_type args)
+        {
+            node_type *prev = NULL;
+            if(search(args,prev))
+                return true;
+            else
+            {
+                node_type *node = cache->make(args);
+                if(prev)
+                    items.insert_after(prev,node);
+                else
+                    items.push_front(node);
+                return true;
+            }
+
+
+        }
+
+        bool contains(param_type args) const
+        {
+            node_type *prev = NULL;
+            return search(args,prev);
+        }
+
+        inline const list_type * operator->() const throw() { return &items; }
+        inline const list_type & operator* () const throw() { return  items; }
+
+
+        
     private:
-        YACK_DISABLE_COPY_AND_ASSIGN(data_set);
-        list_type alive;
-        pool_type cache;
+        list_type items;
+        zcache    cache;
+
+
+        inline bool search(const_type &args, node_type * &prev) const {
+
+            //------------------------------------------------------------------
+            //
+            // look up in the ordered list
+            //
+            //------------------------------------------------------------------
+            assert(NULL==prev);
+            switch(items.size) {
+                case  0: return false;                   // not found,
+                case  1: return search_first(args,prev); // special case
+                default: break;                          // generic case
+            }
+
+            assert(items.size>=2);
+
+            //------------------------------------------------------------------
+            //
+            // compare to head
+            //
+            //------------------------------------------------------------------
+            switch( __sign::of(args,**items.head) ) {
+                case negative: return false;                   // smaller than head
+                case __zero__: prev = items.head; return true; // found at head
+                case positive: break;                          // greater than head
+            }
+
+
+            //------------------------------------------------------------------
+            //
+            // compare to tail
+            //
+            //------------------------------------------------------------------
+            switch( __sign::of(**items.tail,args) ) {
+                case negative: prev = items.tail; return false; // greater than tail
+                case __zero__: prev = items.tail; return true;  // found   at tauil
+                case positive: break;                           // smaller than tail
+            }
+
+            assert(**items.head<args);
+            assert(args<**items.tail);
+
+            exit(0);
+
+            return false;
+        }
+
+        inline bool search_first(const_type &args, node_type * &prev) const {
+            assert(1==items.size);
+            assert(NULL==prev);
+            switch( __sign::of(args,**items.head) ) {
+                case negative: break;
+                case __zero__: prev = items.head; return true;
+                case positive: prev = items.head; return false;
+            }
+            return false;
+        }
+
 
     };
 
