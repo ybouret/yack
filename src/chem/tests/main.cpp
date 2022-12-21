a6de941e4dac2e3f0dbabf8fff03985782e9131b
Yann Bouret <yann.bouret@unice.fr>
2022-12-21 15:11:13 +0100

    cleaning up chem

--- src/chem/tests/main.cpp
+++ src/chem/tests/main.cpp
@@ -7,11 +7,7 @@ YACK_UTEST_DECL(16)
     YACK_UTEST(components);
     YACK_UTEST(outcome);
     YACK_UTEST(eq);
-    YACK_UTEST(composite);
-    YACK_UTEST(reactor);
     YACK_UTEST(diff);
-    YACK_UTEST(ham);
-    YACK_UTEST(balance);
     YACK_UTEST(nexus);
 }
 YACK_UTEST_EXEC()
