4675bf90c9cbae0244c6b00984fadeee054d9cf5
Yann Bouret <yann.bouret@unice.fr>
2022-12-29 08:45:36 +0100

    wip

--- src/core/tests/apex/npd.cpp
+++ src/core/tests/apex/npd.cpp
@@ -1,8 +1,32 @@
 
 #include "yack/apex/npd.hpp"
 #include "yack/utest/run.hpp"
+#include "yack/utest/run.hpp"
+#include "yack/arith/prime.hpp"
 
 using namespace yack;
+
+namespace
+{
+    //! generic is_prime function
+    template <typename T> inline
+    static bool check_prime(const T n, T &i)
+    {
+        if( n == 2 || n == 3)
+            return true;
+
+        if( n <= 1 || (n % 2) == 0 || (n % 3) == 0)
+            return false;
+
+        for(i = 5; i*i <= n; i += 6)
+        {
+            if ( (n%i) == 0 || (n % (i + 2)) == 0)
+                return false;
+        }
+
+        return true;
+    }
+}
 
 YACK_UTEST(apex_npd)
 {
@@ -16,6 +40,21 @@ YACK_UTEST(apex_npd)
         apex::npd d = n;
         std::cerr << d << std::endl;
     }
+
+    size_t i=0;
+    for(size_t n=5;n<=1000;n+=2)
+    {
+        if(check_prime(n,i))
+        {
+            std::cerr << std::setw(8) << n << " @" << i << std::endl;
+        }
+    }
+
+
+
+
+
+
 
 }
 YACK_UDONE()
