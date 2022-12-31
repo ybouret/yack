ff19204ec3c1187f46082896fb248661ab7b9cb4
Yann Bouret <yann.bouret@unice.fr>
2022-12-31 13:36:04 +0100

    wip

--- src/core/tests/apex/npd.cpp
+++ src/core/tests/apex/npd.cpp
@@ -5,6 +5,7 @@
 #include "yack/arith/prime.hpp"
 #include "yack/arith/isqrt.hpp"
 #include "yack/type/utils.hpp"
+#include "yack/ios/ocstream.hpp"
 
 using namespace yack;
 
@@ -12,17 +13,17 @@ namespace
 {
     //! generic is_prime function
     template <typename T> inline
-    static bool check_prime(const T n, T &i)
+    static bool check_prime(const T n, T &i, T &q)
     {
-        if( n == 2 || n == 3)
+        if( n == (q=2) || n == (q=3) )
             return true;
 
-        if( n <= 1 || (n % 2) == 0 || (n % 3) == 0)
+        if( n <= 1 || (n % (q=2) ) == 0 || (n % (q=3) ) == 0)
             return false;
 
         for(i = 5; i*i <= n; i += 6)
         {
-            if ( (n%i) == 0 || (n % (i + 2)) == 0)
+            if ( (n%(q=i)) == 0 || (n % (q=(i + 2))) == 0)
                 return false;
         }
 
@@ -43,15 +44,22 @@ YACK_UTEST(apex_npd)
         std::cerr << d << std::endl;
     }
 
-    size_t i=0;
-    for(size_t n=5;n<=500;n+=2)
+    ios::ocstream fp("chkprm.dat");
+
+    for(size_t n=5;n<=700;n+=2)
     {
-        i=0;
-        if(check_prime(n,i))
+        size_t i=0;
+        size_t q=0;
+        if(check_prime(n,i,q))
         {
             const size_t k = (i-5)/6;
-            std::cerr << std::setw(8) << n << " @" << i << "=5+6*" << k << std::endl;
-
+            const size_t s = isqrt(n);
+            std::cerr << std::setw(8) << n << " @" << i << "=5+6*" << k <<  " : sqrt: " << isqrt(n) << std::endl;
+            fp("%u %u %u %u\n", unsigned(n), unsigned(i), unsigned(s), unsigned(k) );
+        }
+        else
+        {
+            //std::cerr << std::setw(8) << n << " @" << i << " : / " << q << std::endl;
         }
     }
 
