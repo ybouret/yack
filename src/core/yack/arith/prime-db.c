7bb1a785578de258a41b5d28d6cd046b68eb64f6
Yann Bouret <yann.bouret@unice.fr>
2023-01-03 09:31:44 +0100

    typos

--- src/core/yack/arith/prime-db.c
+++ src/core/yack/arith/prime-db.c
@@ -6,12 +6,14 @@ const yack_prime_db_word yack_prime_db_data[] =
 #include "prime-db.inc"
 };
 
-#define YACK_PRIME_DB_BPS 6
-#define YACK_PRIME_DB_SPW ( (sizeof(yack_prime_db_word)*8)/YACK_PRIME_DB_BPS )
-const size_t yack_prime_db_size = sizeof(yack_prime_db_data)/sizeof(yack_prime_db_data[0]);
-const size_t yack_prime_db_bps = YACK_PRIME_DB_BPS;
-const size_t yack_prime_db_spw = YACK_PRIME_DB_SPW;
-const size_t yack_prime_db_num = YACK_PRIME_DB_SPW * yack_prime_db_size;
+
+#define YACK_PRIME_DB_SIZE (sizeof(yack_prime_db_data)/sizeof(yack_prime_db_data[0]))
+#define YACK_PRIME_DB_BPS  6
+#define YACK_PRIME_DB_SPW  ( (sizeof(yack_prime_db_word)*8)/YACK_PRIME_DB_BPS )
+const size_t yack_prime_db_size = YACK_PRIME_DB_SIZE;
+const size_t yack_prime_db_bps  = YACK_PRIME_DB_BPS;
+const size_t yack_prime_db_spw  = YACK_PRIME_DB_SPW;
+const size_t yack_prime_db_num  = YACK_PRIME_DB_SPW * YACK_PRIME_DB_SIZE;
 
 
 void yack_prime_db_expand(uint8_t shift[], yack_prime_db_word u)
