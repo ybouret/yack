c48ece2988762d57f2d312ffb463828da699d5aa
Yann Bouret <yann.bouret@unice.fr>
2023-01-03 13:01:30 +0100

    wip slim, need to change gateway

--- src/core/tests/data/slim.cpp
+++ src/core/tests/data/slim.cpp
@@ -1,10 +1,27 @@
 #include "yack/data/slim/node.hpp"
 #include "yack/utest/run.hpp"
+#include "yack/ptr/auto.hpp"
+#include "yack/string.hpp"
 
 using namespace  yack;
 
 YACK_UTEST(data_slim)
 {
+
+    YACK_SIZEOF(slim_node<string>);
+    YACK_SIZEOF(slim_node<int>);
+
+    auto_ptr< slim_node<string> > ps1 = new slim_node<string>();
+    auto_ptr< slim_node<string> > ps2 = new slim_node<string>("hello",transmogrify);
+    const string world = "world";
+    auto_ptr< slim_node<string> > ps3 = new slim_node<string>(world,transmogrify);
+    auto_ptr< slim_node<string> > ps4 = new slim_node<string>('y',transmogrify);
+
+    std::cerr << ps1 << std::endl;
+    std::cerr << ps2 << std::endl;
+    std::cerr << ps3 << std::endl;
+    std::cerr << ps4 << std::endl;
+
 
 }
 YACK_UDONE()
---
+++ src/core/yack/data/slim/node.hpp
@@ -0,0 +1,66 @@
+//! \file
+
+#ifndef YACK_DATA_SLIM_NODE_INCLUDED
+#define YACK_DATA_SLIM_NODE_INCLUDED 1
+
+#include "yack/object.hpp"
+#include "yack/type/args.hpp"
+#include "yack/type/destruct.hpp"
+#include "yack/type/transmogrify.hpp"
+#include <iostream>
+
+namespace yack
+{
+#define  YACK_SLIM_NODE_CTOR() object(), next(0), prev(0)
+
+    template <typename T>
+    class slim_node : public object
+    {
+    public:
+        //______________________________________________________________________
+        //
+        // types
+        //______________________________________________________________________
+        YACK_DECL_ARGS_(T,type);      //!< aliases
+        typedef transmogrify_t alt_t; //!< helper
+
+
+        //______________________________________________________________________
+        //
+        // C++
+        //______________________________________________________________________
+        template <typename U>
+        inline explicit slim_node(const U &u, const alt_t &) : YACK_SLIM_NODE_CTOR(), data(u)      {} //!< setup from user's args
+        inline explicit slim_node() :                    YACK_SLIM_NODE_CTOR(), data()       {} //!< default setup, if exists
+        inline explicit slim_node(const slim_node &_) :  YACK_SLIM_NODE_CTOR(), data(_.data) {} //!< copy constructor
+        inline virtual ~slim_node() throw() { assert(!next); assert(!prev); }                   //!< cleanup
+
+        //______________________________________________________________________
+        //
+        // methods
+        //______________________________________________________________________
+        inline type       & operator*()       throw() { return               data; } //!< access
+        inline const_type & operator*() const throw() { return (const_type &)data; } //!< access
+
+        slim_node * zcreate()  { return static_cast<slim_node *>( object::zacquire<slim_node>() ); } //!< create zombie node
+        void        zdelete(slim_node *zombie) throw() { assert(zombie); object::zrelease(zombie); } //!< delete zombie node
+
+        inline friend std::ostream & operator<<(std::ostream &os, const slim_node &self) { return os << self.data; }
+
+        //______________________________________________________________________
+        //
+        // members
+        //______________________________________________________________________
+        slim_node *next;
+        slim_node *prev;
+        
+    private:
+        mutable_type data;
+        YACK_DISABLE_ASSIGN(slim_node);
+    };
+
+}
+
+
+#endif
+
