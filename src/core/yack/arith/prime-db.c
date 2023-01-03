be30b6813eb6963b3ba225538b788ed15aadc804
Yann Bouret <yann.bouret@unice.fr>
2023-01-03 09:28:29 +0100

    wip typo

--- src/core/yack/arith/prime-db.c
+++ src/core/yack/arith/prime-db.c
@@ -6,9 +6,11 @@ const yack_prime_db_word yack_prime_db_data[] =
 #include "prime-db.inc"
 };
 
+#define YACK_PRIME_DB_BPS 6
+
 const size_t yack_prime_db_size = sizeof(yack_prime_db_data)/sizeof(yack_prime_db_data[0]);
-const size_t yack_prime_db_bps = 6;
-const size_t yack_prime_db_spw = (sizeof(yack_prime_db_word)*8)/yack_prime_db_bps;
+const size_t yack_prime_db_bps = YACK_PRIME_DB_BPS;
+const size_t yack_prime_db_spw = (sizeof(yack_prime_db_word)*8)/YACK_PRIME_DB_BPS;
 const size_t yack_prime_db_num = yack_prime_db_spw *yack_prime_db_size;
 
 
