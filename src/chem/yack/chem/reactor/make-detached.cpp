414e16f56e4d1c68616387a0f699e6b88bd0931e
Yann Bouret <yann.bouret@unice.fr>
2023-01-13 16:24:17 +0100

    need to keep soft/delimited

--- src/chem/yack/chem/reactor/make-detached.cpp
+++ src/chem/yack/chem/reactor/make-detached.cpp
@@ -18,48 +18,9 @@ namespace yack
         {
             assembly.clear();
             for(const eq_gnode *en=genus->delimited->head;en;en=en->next)
-                assembly << (***en).host;
-        }
-
-
-        void cluster:: make_detached(const xmlog        &xml,
-                                     squads             &platoon,
-                                     const matrix<bool> &detached,
-                                     const char * const  info)
-        {
-            YACK_XMLSUB(xml,info);
-
-            //------------------------------------------------------------------
-            //
-            // prepare resources
-            //
-            //------------------------------------------------------------------
-            const glist &grp = *group;
-            eq_repo_     assembly;
-
-            //------------------------------------------------------------------
-            //
-            // form assembly
-            //
-            //------------------------------------------------------------------
-            for(const gnode *gn=grp->head;gn;gn=gn->next)
-                assembly << gn->host;
-            
-
-            //------------------------------------------------------------------
-            //
-            // build army
-            //
-            //------------------------------------------------------------------
-            platoon.shape(assembly,detached);
-            if(xml.verbose)
             {
-                for(const squad *sq = platoon.head;sq;sq=sq->next)
-                {
-                    *xml << *sq << std::endl;
-                }
+                assembly << (***en).host;
             }
-
         }
 
     }
