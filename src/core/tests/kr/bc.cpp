c8c78ae37a335f8a8b864612b96899059b8889c7
ybouret <yann.bouret@gmail.com>
2023-02-02 18:44:11 +0100

    wip

--- src/core/tests/kr/bc.cpp
+++ src/core/tests/kr/bc.cpp
@@ -4,10 +4,45 @@
 
 #include "yack/string.hpp"
 #include "yack/utest/run.hpp"
-
+#include "yack/check/static.hpp"
 
 using namespace yack;
 using namespace crypto;
+
+template <size_t NDW>
+class teac : public block_cipher
+{
+public:
+    static const size_t block_size = NDW * sizeof(uint32_t);
+    static const size_t dw_per_key = 4;
+    
+    virtual size_t size() const throw() { return block_size; }
+    
+    explicit teac(const memory::ro_buffer &key) throw() :
+    k()
+    {
+        YACK_STATIC_CHECK(NDW>=2,too_small);
+        memset(k,0,sizeof(k));
+        const size_t n = key.measure();
+        if(n>=sizeof(k))
+        {
+            memcpy(k,key.ro_addr(),sizeof(k));
+        }
+        else
+        {
+            memcpy(k,key.ro_addr(),n);
+        }
+    }
+    
+    virtual ~teac() throw() { memset(k,0,sizeof(k)); }
+    
+    virtual const char * name() const throw() { return "TEAC"; }
+    
+private:
+    YACK_DISABLE_COPY_AND_ASSIGN(teac);
+    uint32_t k[dw_per_key];
+};
+
 
 static inline
 void test_codec(block_cipher &enc, block_cipher &dec, randomized::bits &ran)
@@ -53,6 +88,7 @@ YACK_UTEST(kr_bc)
         aes256::decrypter dec(key);
         test_codec(enc,dec,ran);
     }
+    
     
 }
 YACK_UDONE()
