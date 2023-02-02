b2c04656523af5852b24f755bcb5f4ee5013fe1b
ybouret <yann.bouret@gmail.com>
2023-02-02 17:53:34 +0100

    wip

--- src/core/yack/kr/bc/block-cipher.hpp
+++ src/core/yack/kr/bc/block-cipher.hpp
@@ -10,19 +10,36 @@ namespace yack {
     
     namespace crypto
     {
+        //______________________________________________________________________
+        //
+        //
+        //! interface to a block cipher
+        //
+        //______________________________________________________________________
         class block_cipher
         {
         public:
-            virtual ~block_cipher() throw();
-            
-            virtual size_t size() const throw()                   = 0;
-            virtual void   call(void *target, const void *source) = 0;
+            //__________________________________________________________________
+            //
+            // virtual interface
+            //__________________________________________________________________
+            virtual size_t size() const throw()                   = 0; //!< block size in bytes
+            virtual void   call(void *target, const void *source) = 0; //!< process target[block_size] from source[block_size]
 
+            //__________________________________________________________________
+            //
             // non virtual interface
-            size_t         bits() const throw();
+            //__________________________________________________________________
+            size_t         bits() const throw(); //!< 8 * size()
+          
+            //__________________________________________________________________
+            //
+            // C++
+            //__________________________________________________________________
+            virtual ~block_cipher() throw(); //!< cleanup
             
         protected:
-            explicit block_cipher();
+            explicit block_cipher();         //!< setup
             
         private:
             YACK_DISABLE_COPY_AND_ASSIGN(block_cipher);
