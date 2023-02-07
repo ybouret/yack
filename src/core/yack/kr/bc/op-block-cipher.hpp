9aee457513472caaa8f22624a85d09d458d4b8e9
ybouret <yann.bouret@gmail.com>
2023-02-07 10:19:24 +0100

    wip

---
+++ src/core/yack/kr/bc/op-block-cipher.hpp
@@ -0,0 +1,20 @@
+
+//! \file
+
+#ifndef YACK_KR_BLOCK_CIPHER_INCLUDED
+#define YACK_KR_BLOCK_CIPHER_INCLUDED 1
+
+#include "yack/kr/digest.hpp"
+#include "yack/string.hpp"
+
+namespace yack {
+    
+    namespace crypto
+    {
+        //______________________________________________________________________
+        //
+        //
+        //! interface to an operating block cipher
+        //
+        //______________________________________________________________________
+        
