10787cf37d6883bc33d7ee3f56532c62209ce16d
ybouret <yann.bouret@gmail.com>
2023-02-22 22:01:52 +0100

    wip

--- src/core/yack/cameo/add.hpp
+++ src/core/yack/cameo/add.hpp
@@ -9,85 +9,111 @@
 
 namespace yack
 {
-    namespace cameo
-    {
-        //______________________________________________________________________
-        //
-        //
-        //! type comparator for add
-        //
-        //______________________________________________________________________
-        template <typename T> struct add_comparator
-        {
-            //! decreasing absolute value
-            inline int operator()(const T &lhs, const T &rhs) const
-            {
-                const typename scalar_for<T>::type L = math::abs_of(lhs);
-                const typename scalar_for<T>::type R = math::abs_of(rhs);
-                return (L<R) ? 1 : ( (R<L) ? -1 : 0);
-            }
-        };
+	namespace cameo
+	{
+		//______________________________________________________________________
+		//
+		//
+		//! type comparator for add
+		//
+		//______________________________________________________________________
+		template <typename T> struct add_comparator
+		{
+			//! decreasing absolute value
+			inline int operator()(const T& lhs, const T& rhs) const
+			{
+				const typename scalar_for<T>::type L = math::abs_of(lhs);
+				const typename scalar_for<T>::type R = math::abs_of(rhs);
+				return (L < R) ? 1 : ((R < L) ? -1 : 0);
+			}
+		};
 
-        //______________________________________________________________________
-        //
-        //
-        //! addition with priority queue
-        //
-        //______________________________________________________________________
-        template <typename T>
-        class add : public object_type, public pqueue<T, add_comparator<T>, allocator_type>
-        {
-        public:
-            //__________________________________________________________________
-            //
-            // types
-            //__________________________________________________________________
-            YACK_DECL_ARGS(T,type);
-            typedef pqueue<T, add_comparator<T>,allocator_type> self_type;
+		//______________________________________________________________________
+		//
+		//
+		//! addition with priority queue
+		//
+		//______________________________________________________________________
+		template <typename T>
+		class add : public object_type, public pqueue<T, add_comparator<T>, allocator_type>
+		{
+		public:
+			//__________________________________________________________________
+			//
+			// types
+			//__________________________________________________________________
+			YACK_DECL_ARGS(T, type);
+			typedef pqueue<T, add_comparator<T>, allocator_type> self_type;
 
-            using self_type::size;
-            using self_type::pull;
-            using self_type::push_;
+			using self_type::size;
+			using self_type::pull;
+			using self_type::push_;
+			using self_type::resume;
 
-            //__________________________________________________________________
-            //
-            // C++
-            //__________________________________________________________________
-            inline explicit add() noexcept : object_type(), self_type() {}
-            inline virtual ~add() noexcept {}
+			//__________________________________________________________________
+			//
+			// C++
+			//__________________________________________________________________
+			inline explicit add() noexcept : object_type(), self_type() {}
+			inline virtual ~add() noexcept {}
 
-            //__________________________________________________________________
-            //
-            // methods
-            //__________________________________________________________________
-            inline T sum() {
-                switch(size())
-                {
-                    case 0:  return 0;
-                    case 1:  return pull();
-                    default: break;
-                }
-                assert( size() > 1 );
-            REDUCE:
-                    const_type lhs = pull(); assert(size()>=1);
-                    const_type rhs = pull();
-                    const_type tmp = lhs+rhs;
-                    if(size()<=0) return tmp;
-                    push_(tmp);
-                goto REDUCE;
-            }
+			//__________________________________________________________________
+			//
+			// methods
+			//__________________________________________________________________
+			inline T sum() {
+				switch (size())
+				{
+				case 0:  return 0;
+				case 1:  return pull();
+				default: break;
+				}
+				assert(size() > 1);
+			REDUCE:
+				const_type lhs = pull(); assert(size() >= 1);
+				const_type rhs = pull();
+				const_type tmp = lhs + rhs;
+				if (size() <= 0) return tmp;
+				push_(tmp);
+				goto REDUCE;
+			}
 
-            template <typename ITERATOR> inline void toto()
-            {
-            }
+			template <typename ITERATOR>
+			inline T range(ITERATOR i, size_t n)
+			{
+				resume(n);
+				while (n-- > 0) push_(*(i++));
+				return sum();
+			}
+
+			template <typename SEQUENCE>
+			inline T range(SEQUENCE& seq)
+			{
+				return range(seq.begin(), seq.size());
+			}
+
+			template <typename LHS, typename RHS>
+			inline T dot(LHS& lhs, RHS& rhs)
+			{
+				assert(lhs.size() == rhs.size());
+				const size_t n = lhs.size();
+				resume(n);
+				for (size_t i = lhs.size(); i > 0; --i)
+				{
+					const_type l = lhs[i];
+					const_type r = rhs[i];
+					const_type p = l * r;
+					push_(p);
+				}
+				return sum();
+			}
 
 
 
-
-        private:
-            YACK_DISABLE_COPY_AND_ASSIGN(add);
-        };
-    }
+		private:
+			YACK_DISABLE_COPY_AND_ASSIGN(add);
+		};
+	}
 }
 
 #endif
