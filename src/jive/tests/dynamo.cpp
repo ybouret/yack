30f23e226bb007e4ed1397c0d6c263d09e655a71
Yann Bouret <yann.bouret@gmail.com>
2023-03-06 11:29:28 +0100

    wip

--- src/jive/tests/dynamo.cpp
+++ src/jive/tests/dynamo.cpp
@@ -1,6 +1,33 @@
 
-#include "yack/jive/source.hpp"
+#include "yack/jive/parser.hpp"
 #include "yack/utest/run.hpp"
+
+namespace yack
+{
+    namespace jive
+    {
+
+        class dynamo : public parser
+        {
+        public:
+            virtual ~dynamo() noexcept {}
+
+            explicit dynamo() : parser( "dynamo" )
+            {
+                const rule &END = term(';');
+
+                top( agg("dynamo") << END );
+                gv();
+                
+            }
+            
+
+        private:
+            YACK_DISABLE_COPY_AND_ASSIGN(dynamo);
+        };
+
+    }
+}
 
 using namespace yack;
 using namespace jive;
@@ -8,7 +35,15 @@ using namespace jive;
 
 YACK_UTEST(dynamo)
 {
-    
+    dynamo dyn;
+
+    if(argc>1)
+    {
+        const string           fn = argv[1];
+        auto_ptr<jive::module> m  = module::open_file(fn);
+        std::cerr << m << std::endl;
+    }
+
 }
 YACK_UDONE()
 
