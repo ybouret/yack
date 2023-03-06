4faead023581bbe46bd92b579a668eedf963c61e
Yann Bouret <yann.bouret@gmail.com>
2023-03-06 14:07:25 +0100

    wip

--- src/jive/tests/dynamo.cpp
+++ src/jive/tests/dynamo.cpp
@@ -1,6 +1,11 @@
 
 #include "yack/jive/parser.hpp"
 #include "yack/utest/run.hpp"
+#include "yack/jive/syntax/xnode.hpp"
+#include "yack/jive/syntax/translator.hpp"
+
+#include "yack/jive/lexical/plugin/single-line-comment.hpp"
+#include "yack/jive/lexical/plugin/multi-lines-comment.hpp"
 
 namespace yack
 {
@@ -14,11 +19,16 @@ namespace yack
 
             explicit dynamo() : parser( "dynamo" )
             {
-                const rule &END = term(';');
-
-                top( agg("dynamo") << END );
+                const rule &END     = mark(';');
+                const rule &UID     = term("UID","\\.[:word:]+");
+                const rule &UID_    = act("UID_") << UID << END;
+                top( agg("dynamo") << UID_ );
                 gv();
-                
+
+                plug( jive::lexical::cxx_comment::clid, "C++Comment");
+                plug( jive::lexical::c_comments::clid,  "C_Comments");
+                drop("[:blank:]+");
+                endl("[:endl:]");
             }
             
 
@@ -35,13 +45,17 @@ using namespace jive;
 
 YACK_UTEST(dynamo)
 {
+    jive::syntax::rule::verbose = true;
     dynamo dyn;
 
     if(argc>1)
     {
-        const string           fn = argv[1];
-        auto_ptr<jive::module> m  = module::open_file(fn);
-        std::cerr << m << std::endl;
+        const string                  fn = argv[1];
+        auto_ptr<jive::syntax::xnode> tree = dyn.parse(module::open_file(fn));
+        YACK_CHECK(tree.is_valid());
+        tree->gv("tree.dot");
+        jive::syntax::translator xt;
+        xt.walk(*tree,NULL);
     }
 
 }
