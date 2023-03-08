1df678489e41748d6fa8b6a022e9fae9394f3318
Yann Bouret <yann.bouret@unice.fr>
2023-03-08 15:39:56 +0100

    need to rethink weasel

--- src/aqueous/yack/aqueous/weasel/parser.cpp
+++ src/aqueous/yack/aqueous/weasel/parser.cpp
@@ -11,28 +11,31 @@ namespace yack
             {
             }
 
-#define ID "[:alpha:][:word:]*"
+#define RX_ID "[:alpha:][:word:]*"
+#define RX_KN "@[-+^.[:word:]]+"
+
             parser:: parser() : jive::parser("weasel")
             {
                 rule::verbose = true;
-                const rule &pp = term('+');                                 // plus sign
-                const rule &mm = term('-');                                 // minus sign
-                const rule &pc = oom(pp);                                   // positive charge(s)
-                const rule &nc = oom(mm);                                   // negative charge(s)
-                const rule &id = term("id",ID);                             // identifier
-                const rule &sp = agg("sp") << id << opt(choice(pc,nc));     // single species
-                const rule &cc = act("cc") << mark('[') << sp << mark(']'); // component
-                const rule &pm    = choice(pp,mm);                          // \pm
-                const rule &cf    = term("cf","[1-9][0-9]*");               // positive coefficient
-                const rule &oc    = opt(cf);                                // optional coefficient
-                const rule &first = agg("first") << opt(pm) << oc << cc;    // first actor
-                const rule &extra = agg("extra") <<     pm  << oc << cc;    // extra actor(s)
+                const rule &pp    = term('+');                                 // plus sign
+                const rule &mm    = term('-');                                 // minus sign
+                const rule &pc    = oom(pp);                                   // positive charge(s)
+                const rule &nc    = oom(mm);                                   // negative charge(s)
+                const rule &id    = term("id",RX_ID);                          // identifier
+                const rule &sp    = agg("sp") << id << opt(choice(pc,nc));     // single species
+                const rule &cc    = act("cc") << mark('[') << sp << mark(']'); // species as component
+                const rule &pm    = choice(pp,mm);                             // \pm
+                const rule &cf    = term("cf","[1-9][0-9]*");                  // positive coefficient
+                const rule &oc    = opt(cf);                                   // optional coefficient
+                const rule &first = agg("first") << opt(pm) << oc << cc;       // first actor
+                const rule &extra = agg("extra") <<     pm  << oc << cc;       // extra actor(s)
+                const rule &Kn    = term("Kn",RX_KN);
 
                 // equilibrium: id '=' first extra* '@'
                 const rule &eq    =
                 agg("eq") << id << mark('=')
                 << first << zom(extra)
-                << term('K',"@[:word:]+");
+                << Kn;
                 ;
 
                 top( act("weasel") << zom(choice(eq,sp) ) );
