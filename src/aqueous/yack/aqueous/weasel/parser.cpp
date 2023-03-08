1144b15f968fa3a5b12de73406ba92b52ff2a6ec
Yann Bouret <yann.bouret@unice.fr>
2023-03-08 16:03:18 +0100

    wip

--- src/aqueous/yack/aqueous/weasel/parser.cpp
+++ src/aqueous/yack/aqueous/weasel/parser.cpp
@@ -17,12 +17,32 @@ namespace yack
             parser:: parser() : jive::parser("weasel")
             {
                 rule::verbose = true;
+
+                // describe a single species
                 const rule &pp    = term('+');                                 // plus sign
                 const rule &mm    = term('-');                                 // minus sign
                 const rule &pc    = oom(pp);                                   // positive charge(s)
                 const rule &nc    = oom(mm);                                   // negative charge(s)
                 const rule &id    = term("id",RX_ID);                          // identifier
                 const rule &sp    = agg("sp") << id << opt(choice(pc,nc));     // single species
+
+                // describe a component
+                const rule &cc    = act("cc") << mark('[') << sp << mark(']'); // species as component
+
+                // describe actors
+                const rule &cf    = term("cf","[1-9][0-9]*");                  // positive coefficient
+                const rule &oc    = opt(cf);                                   // optional coefficient
+                const rule &fa    = agg("fa") << opt(pp) << oc << cc;          // First Actor
+                const rule &xa    = agg("xa") <<     pp  << oc << cc;          // eXtra Actor
+                const rule &ac    = agg("ac") << fa << zom(xa);                // ACtors
+
+                // describe components lhs/rhs
+                const rule &ca    = choice(term('.'),ac);
+                const rule &cm    = agg("cm") << opt( cat(ca,mark("<=>")) ) << ca;
+                top( agg("weasel") << zom( choice(sp,cm)) );
+
+#if 0
+
                 const rule &cc    = act("cc") << mark('[') << sp << mark(']'); // species as component
                 const rule &pm    = choice(pp,mm);                             // \pm
                 const rule &cf    = term("cf","[1-9][0-9]*");                  // positive coefficient
@@ -39,7 +59,7 @@ namespace yack
                 ;
 
                 top( act("weasel") << zom(choice(eq,sp) ) );
-                
+#endif
                 
                 // lexical only extra
                 drop("[:blank:]+");
