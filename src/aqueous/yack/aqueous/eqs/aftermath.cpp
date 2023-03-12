bc877ba0e26c6b060d774f19a868e735c321200b
Yann Bouret <yann.bouret@gmail.com>
2023-03-12 10:42:40 +0100

    wip API

---
+++ src/aqueous/yack/aqueous/eqs/aftermath.cpp
@@ -0,0 +1,12 @@
+
+#include "yack/aqueous/eqs/aftermath.hpp"
+
+namespace yack
+{
+    namespace aqueous
+    {
+
+    }
+
+}
+
---
+++ src/aqueous/yack/aqueous/eqs/aftermath.hpp
@@ -0,0 +1,23 @@
+//! \file
+
+#ifndef YACK_AQUEOUS_AFTERMATH_INCLUDED
+#define YACK_AQUEOUS_AFTERMATH_INCLUDED 1
+
+#include "yack/aqueous/components.hpp"
+
+namespace yack
+{
+    namespace aqueous
+    {
+
+        class aftermath
+        {
+
+        };
+        
+    }
+
+}
+
+#endif
+
