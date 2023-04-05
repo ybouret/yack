f57baca856be3b18785e3e5b04352c3499dfd3a6
Yann Bouret <yann.bouret@unice.fr>
2023-04-05 12:08:07 +0200

    typo

--- etc/ninja-gcc-icc.sh
+++ etc/ninja-gcc-icc.sh
@@ -1,4 +1,4 @@
-cmake build-gcc    && \
-ninja -C build-gcc && \
-cmake build-icc    && \
+/opt/local/bin cmake build-gcc    && \
+ninja -C build-gcc                && \
+/opt/local/bin/cmake build-icc    && \
 ninja -C build-icc
