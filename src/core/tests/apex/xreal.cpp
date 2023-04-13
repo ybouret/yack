7d3b8da48bf773814593ba8e10803b07512995ef
Yann Bouret <yann.bouret@gmail.com>
2023-04-13 16:23:59 +0200

    wip

--- src/core/tests/apex/xreal.cpp
+++ src/core/tests/apex/xreal.cpp
@@ -1,25 +1,41 @@
 #include "yack/apex/xreal.hpp"
 #include "yack/utest/run.hpp"
 #include "yack/ios/ascii/convert.hpp"
+#include "yack/sequence/vector.hpp"
 
 using namespace yack;
 using namespace apex;
 
 YACK_UTEST(apex_xreal)
 {
-    xreal<float> prod = 1;
+    vector< xreal<float> > xr;
     for(int i=1;i<argc;++i)
     {
         const float        r = ios::ascii::convert::real<float>(argv[i]);
         const xreal<float> f = r;
         std::cerr << r << " => " << f << std::endl;
-        prod *= f;
+        xr << f;
     }
-    std::cerr << "prod=" << prod << std::endl;
+    std::cerr << xr << std::endl;
 
-    xreal<float> a = 100;
-    xreal<float> b = 0.01;
-    std::cerr << "c=" << a*b << std::endl;
+    {
+        xreal<float> prod = 1;
+        for(size_t i=xr.size();i>0;--i) prod *= xr[i];
+        std::cerr << "prod=" << prod << std::endl;
+    }
+
+    for(size_t i=1;i<=xr.size();++i)
+    {
+        const xreal<float> &num = xr[i];
+        for(size_t j=i;j<=xr.size();++j)
+        {
+            const xreal<float> &den = xr[j];
+            if(fabs(den.m)<=0) continue;
+            const xreal<float> q = num / den;
+            const xreal<float> p = q * den;
+            std::cerr << num << "/" << den << " = " << q << " => " << p << std::endl;
+        }
+    }
 
 }
 YACK_UDONE()
