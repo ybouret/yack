b63645403fd689b80e6b1de5f5c5bfb4ae87c890
Yann Bouret <yann.bouret@unice.fr>
2023-04-17 14:04:55 +0200

    wip

--- src/core/tests/cameo/mul.cpp
+++ src/core/tests/cameo/mul.cpp
@@ -3,6 +3,7 @@
 #include "../main.hpp"
 #include "yack/sequence/vector.hpp"
 #include "yack/sequence/list.hpp"
+#include "yack/ordered/roster.hpp"
 
 using namespace yack;
 
@@ -11,10 +12,142 @@ namespace yack
     namespace cameo
     {
 
-        template <typename T>
-        struct mfield;
+        template <typename> class  marked_real;
+        template <typename> struct marked_data;
+
+        template <>
+        struct marked_data<float>
+        {
+            typedef int                info;
+            typedef marked_real<float> real;
+
+            sign_type   operator()(const real &, const real &);
+            static info from(const float x) { int res=0;  (void) frexp(x,&res); return res; }
+        };
+
+        template <>
+        struct marked_data<double>
+        {
+            typedef int                 info;
+            typedef marked_real<double> real;
+
+            static info from(const double x) { int res=0;  (void) frexp(x,&res); return res; }
+            sign_type   operator()(const real &, const real &);
+        };
+
+        template <>
+        struct marked_data<long double>
+        {
+            typedef int                 info;
+            typedef marked_real<long double> real;
+
+            static info from(const long double x) { int res=0;  (void) frexp(x,&res); return res; }
+            sign_type   operator()(const real &, const real &);
+        };
+
         
 
+
+
+        template <typename T>
+        class marked_real
+        {
+        public:
+            YACK_DECL_ARGS(T,type);
+            typedef marked_data<type>        data_type;
+            typedef typename data_type::info info_type;
+
+            inline explicit marked_real(param_type x) :
+            value(x),
+            field( data_type::from(value) )
+            {
+            }
+
+            inline explicit marked_real(const marked_real &other) noexcept :
+            value(other.value),
+            field(other.field)
+            {
+
+            }
+
+            inline friend std::ostream & operator<<(std::ostream &os, const marked_real &self)
+            {
+                os << self.value << "[" << self.field << "]";
+                return os;
+            }
+
+            const_type       value;
+            const info_type  field;
+
+        private:
+            YACK_DISABLE_ASSIGN(marked_real);
+        };
+
+        sign_type marked_data<float>:: operator()(const real &lhs, const real &rhs)
+        {
+            return __sign::of(lhs.field,rhs.field);
+        }
+
+        sign_type marked_data<double>:: operator()(const real &lhs, const real &rhs)
+        {
+            return __sign::of(lhs.field,rhs.field);
+        }
+
+        sign_type marked_data<long double>:: operator()(const real &lhs, const real &rhs)
+        {
+            return __sign::of(lhs.field,rhs.field);
+        }
+
+        template <typename T>
+        class marked_list : public roster< marked_real<T> , marked_data<T> >
+        {
+        public:
+            YACK_DECL_ARGS(T,type);
+            typedef marked_real<T>              real_type;
+            typedef marked_data<T>              data_type;
+            typedef roster<real_type,data_type> self_type;
+
+            inline explicit marked_list() noexcept : self_type()
+            {
+            }
+
+            inline virtual ~marked_list() noexcept
+            {
+            }
+
+            inline void push(param_type args)
+            {
+                const real_type r(args);
+                this->insert(r);
+            }
+
+
+
+        private:
+            YACK_DISABLE_COPY_AND_ASSIGN(marked_list);
+        };
+
+        template <typename T>
+        static inline
+        T generate(randomized::bits &ran)
+        {
+            static const T ten(10);
+            static const T amp(20);
+            const T rxp = ran.symm<T>();
+            const T tmp = (ran.choice() ? -1 : 1) * std::pow(ten, amp*rxp);
+            return tmp;
+        }
+
+
+        template <typename T>
+        static inline void test_marked(randomized::bits &ran)
+        {
+            marked_list<T> Q;
+            Q.push( generate<T>(ran) );
+            Q.push( generate<T>(ran) );
+            Q.push( generate<T>(ran) );
+            std::cerr << "Q=" << Q << std::endl;
+        }
     }
 }
 
@@ -71,6 +204,12 @@ namespace {
 YACK_UTEST(cameo_mul)
 {
 	randomized::rand_ ran;
+
+    cameo::test_marked<float>(ran);
+    cameo::test_marked<double>(ran);
+    cameo::test_marked<long double>(ran);
+
+    return 0;
 
 	{ cameo::mul<float>  xmul; }
 	{ cameo::mul<double> xmul(7); }
