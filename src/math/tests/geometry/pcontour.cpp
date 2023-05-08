b100e68546d823692191ec94a87ebc4858d0f0ec
Yann Bouret <yann.bouret@gmail.com>
2023-05-08 17:24:36 +0200

    bug fixed

--- src/math/tests/geometry/pcontour.cpp
+++ src/math/tests/geometry/pcontour.cpp
@@ -2,6 +2,7 @@
 #include "yack/math/geometry/pcontour.hpp"
 #include "yack/utest/run.hpp"
 #include "yack/math/triplet.hpp"
+#include "yack/string.hpp"
 
 using namespace yack;
 using namespace math;
@@ -9,19 +10,47 @@ using namespace math;
 YACK_UTEST(pcontour)
 {
     YACK_SIZEOF(periodic_contour<double>);
-    periodic_contour<double> shape(3);
 
-    shape[1] = v2d<double>(1,0);
-    shape[2] = v2d<double>(0,1.5);
-    shape[3] = v2d<double>(-2,-0.1);
+    const size_t N = 4;
+
+    periodic_contour<double> shape(N);
+    typedef v2d<double>      vertex;
+
+    for(size_t i=0;i<N;++i)
+    {
+        const double th = (i*numeric<double>::two_pi) / N;
+        shape[i+1] = vertex( cos(th), 0.8 * sin(th) );
+    }
+
 
     shape.update();
 
-    shape.save("shape.dat");
-    shape.save_accel("accel.dat");
     shape.save_speed("speed.dat");
-    shape.save_shape("spline.dat",100);
     shape.save_curv("curv.dat");
+
+    const size_t np = 1000;
+    shape.save("shape0.dat");
+    shape.save_accel("accel0.dat");
+    shape.save_shape("spline0.dat",np);
+
+    for(unsigned iter=1;iter<=5;++iter)
+    {
+        for(size_t k=0;k<10;++k)
+        {
+            for(size_t i=shape.size();i>0;--i)
+            {
+                shape[i] += 0.1 * shape.nvec[i];
+            }
+            shape.update();
+        }
+
+        shape.save_shape( vformat("spline%u.dat",iter), np);
+        shape.save( vformat("shape%u.dat",iter) );
+        shape.save_accel( vformat("accel%u.dat",iter));
+
+    }
+
+
 }
 YACK_UDONE()
 
--- src/math/yack/math/geometry/pcontour.hpp
+++ src/math/yack/math/geometry/pcontour.hpp
@@ -19,6 +19,10 @@ namespace yack
 
         namespace kernel
         {
+            //__________________________________________________________________
+            //
+            //! common class for periodic contours
+            //__________________________________________________________________
             class periodic_contour : public spot_object
             {
             public:
@@ -34,18 +38,31 @@ namespace yack
             };
         }
 
+        //______________________________________________________________________
+        //
+        //
+        //! periodic contour
+        //
+        //______________________________________________________________________
         template <typename T>
         class periodic_contour :
         public kernel::periodic_contour,
         public cxx_array< v2d<T>, kernel::periodic_contour::allocator>
         {
         public:
-            typedef v2d<T>                      vertex;
-            typedef cxx_array<vertex,allocator> vertices;
-            typedef cxx_array<T,allocator>      rvector;
-
+            //__________________________________________________________________
+            //
+            //! definitions
+            //__________________________________________________________________
+            typedef v2d<T>                      vertex;   //!< alias
+            typedef cxx_array<vertex,allocator> vertices; //!< alias
+            typedef cxx_array<T,allocator>      rvector;  //!< alias
             using vertices::size;
 
+            //__________________________________________________________________
+            //
+            //! C++
+            //__________________________________________________________________
             inline explicit periodic_contour(const size_t n) :
             kernel::periodic_contour(n),
             vertices(n),
@@ -60,6 +77,7 @@ namespace yack
             curv(n),
             amax(0),
             smax(0),
+            cmax(0),
             sm(n,n),
             lu(n),
             rhs(n)
@@ -68,6 +86,12 @@ namespace yack
 
             inline virtual ~periodic_contour() noexcept {}
 
+            //__________________________________________________________________
+            //
+            // methods
+            //__________________________________________________________________
+
+            //! update from current points
             inline void update()
             {
                 compute_bar();
@@ -76,6 +100,7 @@ namespace yack
                 local_frame();
             }
 
+            //! save control points
             template <typename FILENAME>
             inline void save(const FILENAME &fn) const
             {
@@ -86,14 +111,18 @@ namespace yack
                     emit(fp,self[i]);
                 emit(fp,self[1]);
                 fp << '\n';
-                for(size_t i=1;i<=n;++i)
+                if(false)
                 {
-                    emit(fp,bar);
-                    emit(fp,self[i]);
-                    fp << '\n';
+                    for(size_t i=1;i<=n;++i)
+                    {
+                        emit(fp,bar);
+                        emit(fp,self[i]);
+                        fp << '\n';
+                    }
                 }
             }
 
+            //! save rescaled acceleration at control points
             template <typename FILENAME>
             inline void save_accel(const FILENAME &fn) const
             {
@@ -110,6 +139,7 @@ namespace yack
                 }
             }
 
+            //! save rescaled speeds at control points
             template <typename FILENAME>
             inline void save_speed(const FILENAME &fn) const
             {
@@ -126,6 +156,7 @@ namespace yack
                 }
             }
 
+            //! save curvature at control points
             template <typename FILENAME>
             inline void save_curv(const FILENAME &fn) const
             {
@@ -136,12 +167,13 @@ namespace yack
                 {
                     vertex p = self[i];
                     emit(fp,p);
-                    p += curv[i] * nvec[i];
+                    p += curv[i] * nvec[i] / cmax;
                     emit(fp,p);
                     fp << '\n';
                 }
             }
 
+            //! using spline interpolation
             vertex operator()(const double th) const
             {
                 static const T one(1);
@@ -154,9 +186,10 @@ namespace yack
                 const vertex d2lo = accel[klo];
                 const vertex d2hi = accel[khi];
                 const vertex dtmp = A*(A*A-one) * d2lo + B*(B*B-one) * d2hi;
-                return A*lo + B*hi + (h*h*dtmp) / six;
+                return A*lo + B*hi +  (h*h*dtmp) / six;
             }
 
+            //! save shape using np>=3 points
             template <typename FILENAME>
             inline void save_shape(const FILENAME &fn, const size_t np) const
             {
@@ -175,17 +208,18 @@ namespace yack
 
             }
 
-            const vertex   bar;    //!< barycenter
-            const rvector  theta;
-            const T        theta_prev;
-            const T        theta_next;
-            const vertices accel;
-            const vertices speed;
-            const vertices tvec;
-            const vertices nvec;
-            const rvector  curv;
-            const double   amax;
-            const double   smax;
+            const vertex   bar;          //!< barycenter
+            const rvector  theta;        //!< angles
+            const T        theta_prev;   //!< theta[0]
+            const T        theta_next;   //!< theta[N+1]
+            const vertices accel;        //!< accelerations
+            const vertices speed;        //!< speeds
+            const vertices tvec;         //!< tangent vector
+            const vertices nvec;         //!< outer normal vector
+            const rvector  curv;         //!< curvature
+            const double   amax;         //!< max |accel|
+            const double   smax;         //!< max |speed|
+            const double   cmax;         //!< max |curv|
 
         private:
             YACK_DISABLE_ASSIGN(periodic_contour);
@@ -194,14 +228,14 @@ namespace yack
             rvector        rhs;
 
 
-            inline double find(size_t &klo,
-                               size_t &khi,
-                               T &A,
-                               T &B,
-                               T  th) const
+            inline T find(size_t &klo,
+                          size_t &khi,
+                          T &A,
+                          T &B,
+                          T  th) const
             {
                 while(th>numeric<T>::two_pi) th -= numeric<T>::two_pi;
-                while(th<0) th += numeric<T>::two_pi;
+                while(th<0)                  th += numeric<T>::two_pi;
 
                 const size_t N = size();
                 if(th<=theta[1])
@@ -209,8 +243,8 @@ namespace yack
                     klo = N;
                     khi = 1;
                     const T h = theta[1] - theta_prev;
-                    A = (theta[1] - th)/h;
-                    B = (th-theta_prev)/h;
+                    A = clamp<T>(0,(theta[1] - th)/h,1);
+                    B = clamp<T>(0,(th-theta_prev)/h,1);
                     return h;
                 }
                 else
@@ -220,8 +254,8 @@ namespace yack
                         klo = N;
                         khi = 1;
                         const T h = theta_next - theta[N];
-                        A = (theta_next - th)/h;
-                        B = (th-theta[N])/h;
+                        A = clamp<T>(0,(theta_next - th)/h,1);
+                        B = clamp<T>(0,(th-theta[N])/h,1);
                         return h;
                     }
                     else
@@ -237,8 +271,8 @@ namespace yack
                         const T tlo = theta[klo];
                         const T thi = theta[khi];
                         const T h   = thi-tlo;
-                        A = (thi-th)/h;
-                        B = (th-tlo)/h;
+                        A =  clamp<T>(0,(thi-th)/h,1);
+                        B =  clamp<T>(0,(th-tlo)/h,1);
                         return h;
                     }
                 }
@@ -263,10 +297,24 @@ namespace yack
                 return std::sqrt(res);
             }
 
+
+            static inline
+            T max_abs_of(const rvector &p) noexcept
+            {
+                T res = std::abs(p[1]);
+                for(size_t i=p.size();i>1;--i)
+                {
+                    const T tmp = std::abs(p[i]);
+                    if(tmp>res) res = tmp;
+                }
+                return res;
+            }
+
+
+
             inline
             void make_speed(const double dth, const size_t i, const size_t ip1) noexcept
             {
-                //const T      dth = theta[ip1]-theta[i];
                 const vertex dp  = (*this)[ip1] - (*this)[i];
                 coerce(speed)[i] = dp/dth - dth*(accel[i]+accel[i]+accel[ip1])/6;
             }
@@ -283,27 +331,31 @@ namespace yack
                     const vertex  &a  = accel[i];
                     coerce(curv[i]) = (s.x * a.y - s.y * a.x)/(sn*sn*sn);
                 }
+                coerce(cmax) = max_abs_of(curv);
             }
 
             void make_spline()
             {
                 // build matrix
                 const size_t n = size();
-                sm[1][1] = (theta[2] - theta_prev)/3;
-                sm[1][2] = (theta[2] - theta[1]  )/6;
-                sm[1][n] = (theta[1] - theta_prev)/6;
+
+                sm.ld(0);
+                sm[1][1] = (theta[2] - theta_prev)/3; assert( sm[1][1] > 0);
+                sm[1][2] = (theta[2] - theta[1]  )/6; assert( sm[1][2] > 0);
+                sm[1][n] = (theta[1] - theta_prev)/6; assert( sm[1][n] > 0);
 
                 for(size_t i=2;i<n;++i)
                 {
-                    sm[i][i-1] = ( theta[i]   - theta[i-1] )/6;
-                    sm[i][i]   = ( theta[i+1] - theta[i-1] )/3;
-                    sm[i][i+1] = ( theta[i+1] - theta[i]   )/6;
+                    sm[i][i-1] = ( theta[i]   - theta[i-1] )/6; assert( sm[i][i-1] > 0);
+                    sm[i][i]   = ( theta[i+1] - theta[i-1] )/3; assert( sm[i][i]   > 0);
+                    sm[i][i+1] = ( theta[i+1] - theta[i]   )/6; assert( sm[i][i+1] > 0);
                 }
 
-                sm[n][n]   = ( theta_next - theta[n-1] )/3;
-                sm[n][n-1] = ( theta[n]   - theta[n-1] )/6;
-                sm[n][1]   = ( theta_next - theta[n]   )/6;
+                sm[n][n]   = ( theta_next - theta[n-1] )/3; assert( sm[n][n] > 0);
+                sm[n][n-1] = ( theta[n]   - theta[n-1] )/6; assert( sm[n][n-1] > 0);
+                sm[n][1]   = ( theta_next - theta[n]   )/6; assert( sm[n][1] > 0);
 
+                std::cerr << "sm=" << sm << std::endl;
                 // decompose matrix
                 if(!lu.build(sm)) raise_singular();
 
@@ -320,7 +372,7 @@ namespace yack
                     lu.solve(sm,rhs);
                     for(size_t i=n;i>0;--i) coerce(accel[i][j]) = rhs[i];
                 }
-                std::cerr << "accel = " << accel << std::endl;
+                //std::cerr << "accel = " << accel << std::endl;
                 coerce(amax) = max_norm_of(accel);
 
                 // compute speed
@@ -344,6 +396,7 @@ namespace yack
                     coerce(theta[i]) = a >=0 ?  a : (a+two_pi);
                 }
                 hsort(coerce(theta),coerce(*this), comparison::increasing<T> );
+                std::cerr << "theta=" << theta << std::endl;
                 coerce(theta_prev) = theta[n] - two_pi;
                 coerce(theta_next) = theta[1] + two_pi;
             }
