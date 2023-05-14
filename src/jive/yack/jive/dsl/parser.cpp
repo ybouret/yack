b6f9b2030708fda5c761e832c2719f24a7aacf3e
Yann Bouret <yann.bouret@gmail.com>
2023-05-14 22:30:28 +0200

    wip

--- src/jive/yack/jive/dsl/parser.cpp
+++ src/jive/yack/jive/dsl/parser.cpp
@@ -18,15 +18,29 @@ namespace yack
 
         dsl_parser:: dsl_parser() : parser("dsl_parser")
         {
+            //------------------------------------------------------------------
+            //
             // top level MODULE
+            //
+            //------------------------------------------------------------------
             compound   &MODULE    = agg("MODULE");
 
+            //------------------------------------------------------------------
+            //
+            // common rules
+            //
+            //------------------------------------------------------------------
             const rule &END       = mark(';');
             const rule &SEP       = mark(':');
-            // a module stat with a MODULE_ID
+
+            //------------------------------------------------------------------
+            //
+            // a module starts with a MODULE_ID
+            //
+            //------------------------------------------------------------------
             MODULE << cat(term("MODULE_ID","[.]" DSL_IDENTIFIER_RX ),END);
 
-
+            
             const rule &RULE_ID = term("RULE_ID",DSL_IDENTIFIER_RX);       // Rule IDentifier
             const rule &JSTRING = load<jive::lexical::jstring>("JSTRING"); // JSTRING
             const rule &RSTRING = load<jive::lexical::jstring>("RSTRING"); // RSTRING
